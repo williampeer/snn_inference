5720dcd912d373974b082f7ecd7bcf362322ef2d
William <williapb@gmail.com>
2020-06-29 21:36:01 +0200

    Implement hand engineered Izhikevich sleep model

--- Models/SleepModels.py
+++ Models/SleepModels.py
@@ -68,3 +68,74 @@ class LIF(nn.Module):
 
         return self.v, self.spiked
 
+
+class IzhikevichStable(nn.Module):
+    def __init__(self):
+        super(IzhikevichStable, self).__init__()
+
+        __constants__ = ['spike_threshold', 'N']
+        self.spike_threshold = T(30.)
+        N = 12
+        self.N = N
+
+        # w_Wake = torch.ones((4, 1)) * torch.cat([T(4 * [0.]), T(4 * [-.5]), T(4 * [-.25])])
+        # w_REM = torch.ones((4, 1)) * torch.cat([T(4 * [0.125]), T(4 * [0.6]), T(4 * [0.])])
+        # w_NREM = torch.ones((4, 1)) * torch.cat([T(4 * [-.21]), T(4 * [-.2125]), T(4 * [0.])])
+        w_Wake = torch.ones((4, 1)) * torch.cat([T(4 * [0.]), T(4 * [-.4]), T(4 * [-.2])])
+        w_REM = torch.ones((4, 1)) * torch.cat([T(4 * [.1]), T(4*[.16]), T(4 * [0.])])
+        w_NREM = torch.ones((4, 1)) * torch.cat([T(4 * [-.168]), T(4 * [-.13]), T(4 * [0.])])
+        w = torch.cat([w_Wake, w_REM, w_NREM])
+        self.w = nn.Parameter(w, requires_grad=False)
+
+        a = torch.cat([T(4*[0.15]), T(4*[0.12]), T(4*[0.1])])
+        self.a = nn.Parameter(a, requires_grad=False)
+        b = torch.cat([T(4*[0.25]), T(4*[0.245]), T(4*[0.25])])
+        self.b = nn.Parameter(b, requires_grad=False)
+        c = torch.cat([T(4*[-65.]), T(4*[-42.]), T(4*[-62.])])
+        self.c = nn.Parameter(c, requires_grad=False)
+        d = torch.cat([T(4*[2.]), T(4*[1.3]), T(4*[2.])])
+        self.d = nn.Parameter(d, requires_grad=False)
+
+        tau_g = torch.cat([T(4*[5.]), T(4*[1.]), T(4*[3.0])])  # synaptic conductance decay constant
+        # tau_g = torch.cat([T(4*[1.]), T(4*[1.0]), T(4*[1.0])])  # synaptic conductance decay constant
+        self.tau_g = nn.Parameter(tau_g, requires_grad=False)
+
+        # pre_spike_sensitivity = torch.cat([T(4 * [6.0]), T(4 * [2.0]), T(4 * [3.])])
+        pre_spike_sensitivity = torch.cat([T(4*[6.0]), T(4*[6.0]), T(4*[6.0])])
+        self.pre_spike_sensitivity = nn.Parameter(pre_spike_sensitivity, requires_grad=False)
+
+        self.v = c * torch.ones((self.N,))
+        self.u = d * torch.ones((self.N,))
+        self.spiked = torch.zeros((self.N,))
+        self.g = torch.zeros((self.N,))
+
+    def reset_hidden_state(self):
+        self.spiked = self.spiked.clone().detach()
+        self.v = self.v.clone().detach()
+        self.u = self.u.clone().detach()
+        self.g = self.g.clone().detach()
+
+    def forward(self, x_in):
+        I = torch.add(self.w.matmul(self.g), x_in)
+        # these constants may be set to free params
+        dv = T(0.04) * torch.pow(self.v, 2) + T(5.) * self.v + T(140.) - self.u + I
+        # self.v = self.v + dv
+        self.v = self.v + 0.5 * dv
+        dv = T(0.04) * torch.pow(self.v, 2) + T(5.) * self.v + T(140.) - self.u + I
+        self.v = self.v + 0.5 * dv
+        # dv = tensor(0.04) * torch.pow(self.v, 2) + tensor(5.) * self.v + tensor(140.) - self.u + I
+        # self.v = self.v + 0.5 * dv
+
+        spiked = (self.v >= self.spike_threshold).float()
+        not_spiked = (spiked - 1.) / -1.  # not op.
+        # self.spiked = torch.sigmoid(self.pre_spike_sensitivity * torch.sub(self.v, self.spike_threshold))
+        self.spiked = spiked
+
+        dg = - torch.div(self.g, self.tau_g)
+        du = torch.mul(torch.abs(self.a), torch.sub(torch.mul(torch.abs(self.b), self.v), self.u))
+
+        self.v = not_spiked * self.v + spiked * self.c
+        self.u = not_spiked * (self.u + du) + spiked * self.d
+        self.g = not_spiked * (self.g + dg) + spiked * torch.ones((self.N,))
+
+        return self.v, self.spiked
---
+++ Test/test_SleepModels.py
@@ -0,0 +1,54 @@
+import torch
+from torch import tensor as T
+import model_util
+from Models import SleepModels
+from experiments import poisson_input
+from plot import plot_neuron, plot_all_spiketrains, plot_spiketrain
+from stats import firing_rate_per_neuron
+
+
+def test_sleep_model(snn):
+    inputs = poisson_input(0.5, t=4000, N=snn.N)
+    membrane_potentials, spikes = model_util.feed_inputs_sequentially_return_spikes_and_potentials(snn, inputs)
+    mean_rates = firing_rate_per_neuron(spikes)
+    print('mean_rates (Poisson): {}'.format(mean_rates))
+    plot_neuron(membrane_potentials.data, title='Neuron plot ({:.2f} spikes) Poisson stim.'.format(spikes.sum()), fname_ext='test_LIF_complex_poisson_input')
+    snn.reset_hidden_state()
+
+    wake_mask = torch.cat([T(4*[1.]), T(4*[0.]), T(4*[0.])])
+    membrane_potentials, spikes_wake = model_util.feed_inputs_sequentially_return_spikes_and_potentials(snn, inputs*wake_mask)
+    # plot_neuron(membrane_potentials.data, title='Neuron plot ({:.2f} spikes) wake stim.'.format(spikes_wake.sum()), fname_ext='test_LIF_complex_poisson_wake_input')
+    mean_rates = firing_rate_per_neuron(spikes_wake)
+    print('mean_rates (wake): {}'.format(mean_rates))
+    snn.reset_hidden_state()
+
+    rem_mask = torch.cat([T(4*[0.]), T(4*[1.0]), T(4*[0.])])
+    membrane_potentials, spikes_rem = model_util.feed_inputs_sequentially_return_spikes_and_potentials(snn, inputs*rem_mask)
+    # plot_neuron(membrane_potentials.data, title='Neuron plot ({:.2f} spikes) REM stim.'.format(spikes_rem.sum()), fname_ext='test_LIF_complex_poisson_rem_input')
+    mean_rates = firing_rate_per_neuron(spikes_rem)
+    print('mean_rates (rem): {}'.format(mean_rates))
+    snn.reset_hidden_state()
+
+    nrem_mask = torch.cat([T(4*[0.]), T(4*[0.]), T(4*[1.0])])
+    membrane_potentials, spikes_nrem = model_util.feed_inputs_sequentially_return_spikes_and_potentials(snn, inputs*nrem_mask)
+    # plot_neuron(membrane_potentials.data, title='Neuron plot ({:.2f} spikes) NREM stim.'.format(spikes_nrem.sum()), fname_ext='test_LIF_complex_poisson_nrem_input')
+    mean_rates = firing_rate_per_neuron(spikes_nrem)
+    print('mean_rates (nrem): {}'.format(mean_rates))
+    snn.reset_hidden_state()
+
+    # zeros = torch.zeros((1000, snn.N))
+    # membrane_potentials_zeros, spikes_zeros = model_util.feed_inputs_sequentially_return_spikes_and_potentials(snn, zeros)
+    # plot_neuron(membrane_potentials_zeros.data, title='Neuron plot ({:.2f} spikes) no stimulus'.format(spikes_zeros.sum()), fname_ext='test_LIF_complex_no_input')
+    # mean_rates_zeros = firing_rate_per_neuron(spikes_zeros)
+    # print('mean_rates_zeros: {}'.format(mean_rates_zeros))
+
+    plot_spiketrain(spikes, title='Test {} spiketrain Poisson input'.format(snn.__class__.__name__), uuid='test_LIF_complex')
+    # plot_spiketrains_side_by_side(spikes_rem, spikes_nrem, 'test_LIF_complex', title='Test {} spiketrains REM & NREM input'.format(snn.__class__.__name__), legend=['REM', 'NREM'])
+    # plot_all_spiketrains([spikes, spikes_wake, spikes_rem, spikes_nrem], 'test_{}_complex'.format(snn.__class__.__name__), title='Test {} spiketrains'.format(snn.__class__.__name__), legend=['Poisson', 'Wake', 'REM', 'NREM'])
+
+
+# snn = SleepModels.LIF()
+# test_sleep_model(snn)
+
+snn = SleepModels.IzhikevichStable()
+test_sleep_model(snn)
