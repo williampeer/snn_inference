WIP: Fix backprop. for weights(!) GLIF, fix rates not optimised

Models/LIF.py
@@ -4,8 +4,7 @@ from torch import tensor as T
 
 
 class LIF(nn.Module):
-    def __init__(self, device, parameters, tau_m=1.0, tau_g=2.0, v_rest=-65., N=10, w_mean=0.15, w_var=0.25,
-                 R_I=42.):
+    def __init__(self, device, parameters, tau_m=1.0, tau_g=2.0, v_rest=-65., N=10, w_mean=0.15, w_var=0.25, R_I=42.):
         super(LIF, self).__init__()
         # self.device = device
 
@@ -23,10 +22,8 @@ class LIF(nn.Module):
                     w_mean = float(parameters[key])
                 elif key == 'w_var':
                     w_var = float(parameters[key])
-                elif key == 'pre_activation_coefficient':
-                    pre_activation_coefficient = float(parameters[key])
-                elif key == 'post_activation_coefficient':
-                    post_activation_coefficient = float(parameters[key])
+                elif key == 'R_I':
+                    R_I = float(parameters[key])
 
         __constants__ = ['spike_threshold', 'N']
         self.spike_threshold = T(30.)

Models/LIF_R_ASC_AT.py
@@ -24,6 +24,8 @@ class GLIF(nn.Module):
                     w_mean = float(parameters[key])
                 elif key == 'w_var':
                     w_var = float(parameters[key])
+                elif key == 'R_I':
+                    R_I = float(parameters[key])
 
         __constants__ = ['N', 'v_rest', 'delta_theta_s', 'b_s', 'a_v', 'b_v', 'theta_inf']
         self.N = N
@@ -46,7 +48,9 @@ class GLIF(nn.Module):
         self.w = nn.Parameter(rand_ws, requires_grad=True)  # initialise with positive weights only
         # self.v_rest = nn.Parameter(T(N * [v_rest]), requires_grad=True)
         self.tau_m = nn.Parameter(T(N * [tau_m]), requires_grad=True)
-        self.tau_g = nn.Parameter(T(N * [tau_g]), requires_grad=True)
+        # self.tau_m = T(N * [tau_m])
+        # self.tau_g = nn.Parameter(T(N * [tau_g]), requires_grad=True)
+        # self.R_I = nn.Parameter(T(N * [T(R_I)]), requires_grad=True)
 
         self.R_I = T(R_I)
         self.f_v = T(f_v)
@@ -67,30 +71,27 @@ class GLIF(nn.Module):
         I = x_in + self.w.matmul(self.I_additive)
 
         dv = (self.v_rest - self.v + I * self.R_I) / self.tau_m
-        v_prev = self.v.clone()
-        self.v = self.v + dv
+        # v_prev = self.v.clone()
+        # self.v = self.v + dv
+        v_next = self.v + dv
 
         # differentiable
-        self.spiked = torch.sigmoid(torch.sub(self.v, (self.theta_s + self.theta_v)))
+        self.spiked = torch.sigmoid(torch.sub(v_next, (self.theta_s + self.theta_v)))
 
-        # "filters"
-        spiked = (self.v >= self.theta_s).float()  # thresholding when spiked isn't use for grad.s (non-differentiable)
+        # NB: Non-differentiable terms & "filters"
+        spiked = (v_next >= self.theta_s).float()  # thresholding when spiked isn't use for grad.s (non-differentiable)
         not_spiked = (spiked - 1.) / -1.  # flips the boolean mat.
 
-        # ones_g = torch.ones_like(self.tau_g)
-        # self.g = (ones_g - ones_g / self.tau_g) * self.g  # g = g - g/tau_g
+        self.theta_s = (1 - self.b_s) * self.theta_s + spiked * self.delta_theta_s
+        d_theta_v = self.a_v * (self.v - self.v_rest) - self.b_v * (self.theta_v - self.theta_inf)
+        self.theta_v = self.theta_v + not_spiked * d_theta_v
 
-        self.theta_s = (1 - self.b_s) * self.theta_s
+        # I_additive_decayed = (torch.ones_like(self.k_I_l) - self.k_I_l) * self.I_additive
+        # self.I_additive = I_additive_decayed + spiked * self.I_A
+        self.I_additive = (torch.ones_like(self.k_I_l) - self.k_I_l) * self.I_additive + self.spiked * self.I_A
 
         # Sample values: f_v = 0.15; delta_V = 12.
-        v_reset = self.v_rest + self.f_v * (v_prev - self.v_rest) - self.delta_V
-        self.v = spiked * v_reset + not_spiked * self.v
-        self.theta_s = spiked * (self.theta_s + self.delta_theta_s) + not_spiked * (self.theta_s)
-        d_theta_v = self.a_v * (self.v - self.v_rest) - self.b_v * (self.theta_v - self.theta_inf)
-        self.theta_v = spiked * (self.theta_v) + not_spiked * (self.theta_v + d_theta_v)
-
-        I_additive_decayed = (torch.ones_like(self.k_I_l) - self.k_I_l) * self.I_additive
-        self.I_additive = spiked * (self.I_additive + self.I_A) + not_spiked * I_additive_decayed
-        # self.g = spiked * torch.ones_like(self.g) + not_spiked * self.g
+        v_reset = self.v_rest + self.f_v * (self.v - self.v_rest) - self.delta_V
+        self.v = spiked * v_reset + not_spiked * v_next  # spike reset
 
         return self.v, self.spiked

Test/test_LIF_SNN.py
@@ -6,8 +6,8 @@ from Models.LIF import LIF
 from experiments import poisson_input, zip_dicts, randomise_parameters
 from plot import plot_neuron, plot_spiketrains_side_by_side
 
-static_parameters = {'N': 12, 'w_mean': 0.2, 'w_var': 0.3, 'pre_activation_coefficient': 2.0, 'post_activation_coefficient': 100.0}
-free_parameters = {'tau_m': 2.0, 'tau_g': 2.0, 'v_rest': -60.0}
+static_parameters = {'N': 3, 'w_mean': 0.2, 'w_var': 0.3, 'R_I': 45.}
+free_parameters = {'tau_m': 1.0, 'tau_g': 2.0, 'v_rest': -60.0}
 
 snn = LIF(device='cpu', parameters=zip_dicts(static_parameters, free_parameters))
 
@@ -15,13 +15,13 @@ inputs = poisson_input(0.5, t=500, N=static_parameters['N'])
 # inputs = torch.cat((torch.tensor([12*[4.]]), torch.zeros((100, static_parameters['N']))))
 # inputs = 4. * torch.ones((500, static_parameters['N']))
 membrane_potentials, spikes = model_util.feed_inputs_sequentially_return_spikes_and_potentials(snn, inputs)
-plot_neuron(membrane_potentials.data, title='LIF_test neuron plot ({:.2f} spikes)'.format(spikes.sum()), fname_ext='test_LIF_test_poisson_input')
+plot_neuron(membrane_potentials.data, title='LIF_test neuron plot ({:.2f} spikes)'.format(spikes.sum()), fname_ext='test_LIF_poisson_input')
 
 zeros = torch.zeros_like(inputs)
 membrane_potentials_zeros, spikes_zeros = model_util.feed_inputs_sequentially_return_spikes_and_potentials(snn, zeros)
-plot_neuron(membrane_potentials_zeros.data, title='LIF_test neuron plot ({:.2f} spikes)'.format(spikes_zeros.sum()), fname_ext='test_LIF_test_no_input')
+plot_neuron(membrane_potentials_zeros.data, title='LIF_test neuron plot ({:.2f} spikes)'.format(spikes_zeros.sum()), fname_ext='test_LIF_no_input')
 
-plot_spiketrains_side_by_side(spikes, spikes_zeros, 'test_LIF_test', title='Test LIF_test spiketrains random and zero input')
+plot_spiketrains_side_by_side(spikes, spikes_zeros, 'test_LIF', title='Test LIF_test spiketrains random and zero input')
 
 tau_vr = torch.tensor(2.0)
 loss = spike_metrics.van_rossum_dist(spikes, spikes_zeros, tau=tau_vr)

Test/test_SNN_fitting_stability.py
@@ -1,23 +1,23 @@
 import torch
 
-import Log
-from Models import LIF, Izhikevich, BaselineSNN
+from Models.LIF import LIF
+from Models.LIF_R_ASC_AT import GLIF
 from Test import TestLog
-from experiments import poisson_input, train_test_split
+from experiments import poisson_input, zip_dicts
 from fit import fit_mini_batches
 from model_util import generate_model_data
 from plot import plot_neuron, plot_losses
 
 
-def test_stability_with_matching_configurations_deprecated(model, gen_model, input_coefficient, rate_factor, tau_vr, learn_rate):
+def test_stability_with_matching_configurations_deprecated(model, gen_model, input_coefficient, rate_factor, tau_vr, learn_rate, optim):
     t=4000
 
     poisson_rate = torch.tensor(rate_factor)
     gen_inputs = input_coefficient * poisson_input(poisson_rate, t, gen_model.N)
     gen_membrane_potentials, gen_spiketrain = generate_model_data(model=gen_model, inputs=gen_inputs)
 
-    optims = [torch.optim.SGD(list(model.parameters()), lr=learn_rate),
-              torch.optim.SGD([poisson_rate], lr=learn_rate)]
+    optims = [optim(list(model.parameters()), lr=learn_rate),
+              optim([poisson_rate], lr=learn_rate)]
     for neur_ind in range(gen_membrane_potentials.shape[1]):
         plot_neuron(gen_membrane_potentials[:, neur_ind].data, title='Generative neuron model #{}'.format(neur_ind),
                     fname_ext='_test_1_neuron_{}'.format(neur_ind))
@@ -37,7 +37,7 @@ def test_stability_with_matching_configurations_deprecated(model, gen_model, inp
     return avg_batch_loss
 
 
-def test_stability_with_matching_configurations(model, gen_model, input_coefficient, rate_factor, tau_vr, learn_rate, optim):
+def test_stability_with_matching_configurations(model, gen_model, input_coefficient, rate_factor, tau_vr, learn_rate, optim, train_iter_cap):
     t=4000
 
     gen_model_rate = torch.tensor(rate_factor)
@@ -47,12 +47,15 @@ def test_stability_with_matching_configurations(model, gen_model, input_coeffici
 
     batch_losses = []
     train_iter = 0; avg_batch_loss = 10
-    while train_iter < 100 and avg_batch_loss > 5.0:
+    while train_iter < train_iter_cap and avg_batch_loss > 5.0:
         gen_inputs = input_coefficient * poisson_input(gen_model_rate, t, gen_model.N)
         gen_model.reset_hidden_state()
-        gen_membrane_potentials, targets = generate_model_data(model=gen_model, inputs=gen_inputs)
         # for gen spiketrain this may be thresholded to binary values:
+        gen_membrane_potentials, targets = generate_model_data(model=gen_model, inputs=gen_inputs)
+        gen_membrane_potentials = gen_membrane_potentials.clone().detach()
         gen_spiketrain = targets.clone().detach()
+        plot_neuron(gen_membrane_potentials.data, title='LIF_test neuron plot ({:.2f} spikes)'.format(gen_spiketrain.sum()), fname_ext='test_LIF_poisson_input')
+        assert gen_spiketrain.sum() > 10., "gen spike train had almost no spike. #spikes: {}".format(gen_spiketrain.sum())
 
         # for neur_ind in range(gen_membrane_potentials.shape[1]):
         #     plot_neuron(gen_membrane_potentials[:, neur_ind].data, title='Generative neuron model #{}'.format(neur_ind),
@@ -76,17 +79,16 @@ def test_stability_with_matching_configurations(model, gen_model, input_coeffici
         train_iter += 1
         batch_losses.append(avg_batch_loss)
 
-    plot_losses(batch_losses, [], 'test_SNN_fitting_stability',
-                custom_title='Avg. batch losses ({}, lr={})'.format(optim.__name__, learn_rate))
+    plot_losses(batch_losses, [], 1, 'test_SNN_fitting_stability',
+                custom_title='Avg. batch loss ({}, {}, lr={})'.format(model.__name__, optim.__name__, learn_rate))
 
 
-gen_model = BaselineSNN.BaselineSNN(device='cpu', parameters={}, N=12, w_mean=0.8, w_var=0.6)
-model = BaselineSNN.BaselineSNN(device='cpu', parameters={}, N=12, w_mean=0.8, w_var=0.6)
-tau_vr = torch.tensor(20.0)
-learn_rate = 0.1
+
+# gen_model = BaselineSNN.BaselineSNN(device='cpu', parameters={}, N=12, w_mean=0.8, w_var=0.6)
+# model = BaselineSNN.BaselineSNN(device='cpu', parameters={}, N=12, w_mean=0.8, w_var=0.6)
 # test_stability_with_matching_configurations_deprecated(gen_model, model, input_coefficient=1.0, rate_factor=0.7, tau_vr=tau_vr, learn_rate=0.05)
-test_stability_with_matching_configurations(gen_model, model, input_coefficient=1.0, rate_factor=0.7, tau_vr=tau_vr,
-                                            learn_rate=learn_rate, optim=torch.optim.SGD)
+# test_stability_with_matching_configurations(gen_model, model, input_coefficient=1.0, rate_factor=0.7, tau_vr=tau_vr,
+#                                             learn_rate=learn_rate, optim=torch.optim.SGD)
 
 # gen_model = LIF.LIF(device='cpu', parameters={}, tau_m=6.5, N=3, w_mean=0.2, w_var=0.25)
 # model = LIF.LIF(device='cpu', parameters={}, tau_m=6.5, N=3, w_mean=0.2, w_var=0.25)
@@ -102,3 +104,20 @@ test_stability_with_matching_configurations(gen_model, model, input_coefficient=
 # model = Izhikevich.Izhikevich_constrained(device='cpu', parameters={}, N=3, a=0.1, b=0.28, w_mean=0.1, w_var=0.25)
 # test_stability_with_matching_configurations_and_training_input_noise(gen_model, model, rate_factor=0.25)
 # test_stability_with_matching_configurations_different_training_input_noise(gen_model, model, rate_factor=0.25)
+
+tau_vr = torch.tensor(5.0)
+learn_rate = 0.1
+static_parameters = {'N': 3, 'w_mean': 0.2, 'w_var': 0.3}
+free_parameters = {'tau_g': 2.0}
+params = zip_dicts(static_parameters, free_parameters)
+
+gen_model = LIF(device='cpu', parameters=params, tau_m=1.0, R_I=42.)
+model = GLIF(device='cpu', parameters=params, tau_m=4.0, R_I=20.)
+# model = LIF(device='cpu', parameters=params)
+# test_stability_with_matching_configurations_deprecated(model, gen_model, 1., 0.7, tau_vr, learn_rate, optim=torch.optim.Adam)
+test_stability_with_matching_configurations_deprecated(model, gen_model, 1., 0.7, tau_vr, learn_rate, optim=torch.optim.SGD)
+
+# test_stability_with_matching_configurations(gen_model, model, input_coefficient=1.0, rate_factor=0.7, tau_vr=tau_vr,
+#                                             learn_rate=learn_rate, optim=torch.optim.SGD, train_iter_cap=15)
+# test_stability_with_matching_configurations(gen_model, model, input_coefficient=1.0, rate_factor=0.7, tau_vr=tau_vr,
+#                                             learn_rate=learn_rate, optim=torch.optim.Adam, train_iter_cap=15)

fit.py
@@ -17,11 +17,11 @@ def fit_mini_batches(model, inputs, target_spiketrain, tau_van_rossum, current_r
     batch_losses = model.N * [[]]
     loss = None; cur_inputs = None; loss_per_node = False
     for batch_i in range(batch_N):
-        model.reset_hidden_state()
-        current_rate = torch.abs(current_rate.clone().detach())
-        for optim in optimisers:
-            optim.zero_grad()
         print('batch #{}'.format(batch_i))
+
+        model.reset_hidden_state()
+        # current_rate = current_rate.clone().detach()
+        # current_rate = torch.abs(current_rate.clone().detach())
 
         if inputs is not None:
             spikes = model_util.feed_inputs_sequentially_return_spiketrain(model, inputs[batch_size*batch_i:batch_size*(batch_i+1)])
@@ -51,6 +51,10 @@ def fit_mini_batches(model, inputs, target_spiketrain, tau_van_rossum, current_r
             raise NotImplementedError("Loss function not supported.")
 
         print('batch loss: {}'.format(loss))
+
+        for optim in optimisers:
+            optim.zero_grad()
+
         if loss_per_node:
             for l_i in range(len(loss)):
                 loss[l_i].backward(retain_graph=True)