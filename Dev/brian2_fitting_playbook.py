b9a2596ade7c2659cc56b95531ca4259595475d1
williampeer <williapb@gmail.com>
2020-09-16 21:10:36 +0100

    WIP: Implement b2f

--- Dev/brian2_fitting_playbook.py
+++ Dev/brian2_fitting_playbook.py
@@ -1,6 +1,7 @@
-import brian2modelfitting as b2f
 import data_util
 
+import brian2modelfitting as b2f
+from brian2modelfitting import *
 from brian2 import *
 
 # from Dev.brian_GLIF_model_setup import *
@@ -29,7 +30,7 @@ _, first_outputs = data_util.get_spike_train_matrix(index_last_step=0, advance_b
 
 GLIF_eqs = '''
 dv/dt = ((G * (E_L - v) + R_I * I) / C_m)/tau : volt
-dtheta_v/dt = (a_v * (v - E_L) - b_v * (theta_v - theta_inf))/tau : volt
+dtheta_v/dt = (a_v * (v - E_L) - b_v * (theta_v - theta_innf))/tau : volt
 dtheta_s/dt = (- b_s * theta_s)/tau : volt
 C_m : 1 (constant)
 G : 1 (constant)
@@ -41,20 +42,27 @@ a_v : 1
 E_L : volt
 delta_theta_s : volt
 delta_V : volt
-theta_inf : volt
+theta_innf : volt
 '''
 reset = '''
 v = E_L + f_v * (v - E_L) - delta_V
 theta_s = theta_s - b_s * theta_s + delta_theta_s
 '''
 
-init_params = {'C_m': 1.5, 'G': 0.8, 'R_I': 18., 'E_L': -60.,
-               'delta_theta_s': 25., 'b_s': 0.4, 'f_v': 0.14, 'delta_V': 12.,
-               'b_v': 0.5, 'a_v': 0.5, 'theta_inf': -25.}
+init_params = {'C_m': 1.5, 'G': 0.8, 'R_I': 18.*ohm, 'E_L': -60.*mV,
+               'delta_theta_s': 25.*mV, 'b_s': 0.4, 'f_v': 0.14, 'delta_V': 12.*mV,
+               'b_v': 0.5, 'a_v': 0.5, 'theta_innf': -25.*mV}
 fitter = b2f.fitter.SpikeFitter(GLIF_eqs, input=first_inputs, output=first_outputs, dt=0.5*ms, reset=reset,
                                 threshold='v>30*mV', refractory=False,
                                 n_samples=100, method='euler', param_init=init_params,
                                 penalty=None, use_units=True)
 
-results, error = fitter.fit(n_rounds=2, optimizer=b2f.SkoptOptimizer('ET'),
-                            metric=b2f.GammaFactor(dt=0.1*ms, delta=1*ms))
+results, error = fitter.fit(n_rounds=2,
+                            optimizer=SkoptOptimizer(),
+                            # optimizer=SkoptOptimizer(method='GP', acq_func='LCB'),
+                            # optimizer=NevergradOptimizer(method='PSO'),
+                            metric=GammaFactor(delta=1*ms, time=t*ms),
+                            C_m=[1.0, 2.0], G=[0.3, 1.0], R_I=[15.*ohm, 30.*ohm], f_v=[0.01, 0.99],
+                            delta_V=[1.*mV, 65.*mV], E_L=[-65.*mV, -52.*mV], b_v=[0.01, 0.99], a_v=[0.01, 0.99],
+                            b_s=[0.1, 0.9], delta_theta_s=[1.*mV, 30.*mV],
+                            theta_innf=[-40.*mV, -1.*mV])
