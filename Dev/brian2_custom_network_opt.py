Setup silent brian model, store model, fix param updates, separate logs

Dev/brian2_custom_network_opt.py
@@ -1,6 +1,4 @@
-import nevergrad as ng
 import torch
-from nevergrad.functions import MultiobjectiveFunction
 
 from Dev.brian_GLIF_model_setup import *
 from Dev.setup_data_for_brian import *
@@ -8,26 +6,25 @@ from Log import Logger
 from eval import calculate_loss
 from gf_metric import compute_gamma_factor_for_lists, get_spikes
 
+logger = Logger(log_fname='brian2_network_nevergrad_optimization')
+
 
 def run_simulation_multiobjective(w, C_m, G, R_I, f_v, f_I, E_L, t_interval=time_interval*ms):
-    restore()
+    restore('init')
 
-    synapses.w = w
-    neurons.f_I = f_I
-    neurons.C_m = C_m
-    neurons.G = G
-    neurons.R_I = R_I*ohm
-    neurons.f_v = f_v
-    neurons.E_L = E_L*mV
+    synapses.set_states({ 'w': w })
+    neurons.set_states({ 'f_I': f_I, 'C_m': C_m, 'G': G, 'R_I': R_I*ohm, 'f_v': f_v, 'E_L': E_L*mV })
 
     in_grp.set_spikes(np.reshape(input_indices, (-1,)), np.reshape(input_times*ms, (-1,)))
     spikemon = SpikeMonitor(neurons[:], 'v', record=True)
 
     run(t_interval)
-    print('#spikes in simulation:', spikemon.num_spikes)
+    if spikemon.num_spikes == 0:
+        logger.log("------------- WARN: no spikes in spikes observed")
 
+    rand_sample_index = int(0.6 * np.random.rand() * spike_times.shape[0])
     m_spike_times = get_spikes(spikemon)
-    _, target_spike_times = data_util.get_spike_times_list(index_last_step=int(0.6 * np.random.rand() * spike_times.shape[0]),
+    _, target_spike_times = data_util.get_spike_times_list(index_last_step=rand_sample_index,
                                                            advance_by_t_steps=time_interval, spike_times=spike_times,
                                                            spike_indices=spike_indices, node_numbers=spike_node_indices)
     t_spike_times = data_util.scale_spike_times(target_spike_times)  # ms to seconds
@@ -35,30 +32,28 @@ def run_simulation_multiobjective(w, C_m, G, R_I, f_v, f_I, E_L, t_interval=time
 
     brian_model_spike_train = data_util.convert_brian_spike_train_dict_to_boolean_matrix(spikemon.spike_trains(), t_max=t_interval/ms)
     brian_model_spike_train = torch.tensor(brian_model_spike_train, dtype=torch.float)
-    _, targets = data_util.get_spike_train_matrix(index_last_step=int(0.6 * np.random.rand() * spike_times.shape[0]),
+    _, targets = data_util.get_spike_train_matrix(index_last_step=rand_sample_index,
                                                   advance_by_t_steps=time_interval, spike_times=spike_times,
                                                   spike_indices=spike_indices, node_numbers=spike_node_indices)
     vr_dist = np.float(calculate_loss(brian_model_spike_train, targets, loss_fn='van_rossum_dist', tau_vr=3.0))
     poisson_nll = np.float(calculate_loss(brian_model_spike_train, targets, loss_fn='poisson_nll'))
+
+    logger.log('current losses, rand_sample_index: {}'.format(rand_sample_index), params=[vr_dist, poisson_nll, gf])
     return [vr_dist, poisson_nll, gf]
 
 
-def run_simulation_gamma_factor(w, C_m, G, R_I, f_v, f_I, E_L, t_interval=4000*ms, loss_fn='van_rossum_dist'):
-    restore()
+def run_simulation_for(w, C_m, G, R_I, f_v, f_I, E_L, loss_fn, t_interval=4000*ms):
+    restore('init')
 
-    synapses.w = w
-    neurons.f_I = f_I
-    neurons.C_m = C_m
-    neurons.G = G
-    neurons.R_I = R_I*ohm
-    neurons.f_v = f_v
-    neurons.E_L = E_L*mV
+    synapses.set_states({ 'w': w })
+    neurons.set_states({ 'f_I': f_I, 'C_m': C_m, 'G': G, 'R_I': R_I*ohm, 'f_v': f_v, 'E_L': E_L*mV })
 
     in_grp.set_spikes(np.reshape(input_indices, (-1,)), np.reshape(input_times*ms, (-1,)))
     spikemon = SpikeMonitor(neurons[:], 'v', record=True)
 
     run(t_interval)
-    print('#spikes in simulation:', spikemon.num_spikes)
+    if spikemon.num_spikes == 0:
+        logger.log("------------- WARN: no spikes in spikes observed")
 
     if loss_fn == 'gamma_factor':
         m_spike_times = get_spikes(spikemon)
@@ -67,7 +62,9 @@ def run_simulation_gamma_factor(w, C_m, G, R_I, f_v, f_I, E_L, t_interval=4000*m
             advance_by_t_steps=time_interval, spike_times=spike_times,
             spike_indices=spike_indices, node_numbers=spike_node_indices)
         t_spike_times = data_util.scale_spike_times(target_spike_times)  # ms to seconds
-        return compute_gamma_factor_for_lists(m_spike_times, t_spike_times, time=t_interval, delta=1 * ms)
+        loss = compute_gamma_factor_for_lists(m_spike_times, t_spike_times, time=t_interval, delta=1 * ms)
+        logger.log('loss_fn: gamma_factor, loss: {:3.3f}'.format(loss))
+        return loss
     elif loss_fn in ['van_rossum_dist', 'poisson_nll', 'kl_div']:
         brian_model_spike_train = data_util.convert_brian_spike_train_dict_to_boolean_matrix(spikemon.spike_trains(),
                                                                                              t_max=t_interval / ms)
@@ -76,30 +73,6 @@ def run_simulation_gamma_factor(w, C_m, G, R_I, f_v, f_I, E_L, t_interval=4000*m
             index_last_step=int(0.6 * np.random.rand() * spike_times.shape[0]),
             advance_by_t_steps=time_interval, spike_times=spike_times,
             spike_indices=spike_indices, node_numbers=spike_node_indices)
-        return np.float(calculate_loss(brian_model_spike_train, targets, loss_fn=loss_fn, tau_vr=3.0))
-
-
-N = 12
-w_mean = 0.3; w_var = 0.5; rand_ws = (w_mean - w_var) + 2 * w_var * np.random.random((N**2))
-instrum = ng.p.Instrumentation(w=ng.p.Array(init=rand_ws).set_bounds(-1., 1.),
-                               f_I=ng.p.Array(init=0.4 * np.ones((N,))).set_bounds(0.01, 0.99),
-                               C_m=ng.p.Array(init=1.5 * np.ones((N,))).set_bounds(1., 3.),
-                               G=ng.p.Array(init=0.8 * np.ones((N,))).set_bounds(0.01, 0.99),
-                               R_I=ng.p.Array(init=18. * np.ones((N,))).set_bounds(10., 30.),
-                               f_v=ng.p.Array(init=0.14 * np.ones((N,))).set_bounds(0.01, 0.99),
-                               E_L=ng.p.Array(init=-65. * np.ones((N,))).set_bounds(-90., -30.))
-
-# TODO: PSO, NGO, CMA(?)
-optimizer = ng.optimizers.DE(parametrization=instrum, budget=10)
-
-logger = Logger(log_fname='brian2_network_nevergrad_optimization')
-logger.log('setup experiment with the optimizer {}'.format(optimizer.__str__()))
-
-multiobjective_fn = MultiobjectiveFunction(multiobjective_function=run_simulation_multiobjective)
-recommendation = optimizer.minimize(multiobjective_fn, verbosity=2)
-
-logger.log('recommendation.value: {}'.format(recommendation.value))
-
-logger.log("Random subset:", optimizer.pareto_front(2, subset="random"))
-logger.log("Loss-covering subset:", optimizer.pareto_front(2, subset="loss-covering"))
-logger.log("Domain-covering subset:", optimizer.pareto_front(2, subset="domain-covering"))
+        loss = np.float(calculate_loss(brian_model_spike_train, targets, loss_fn=loss_fn, tau_vr=3.0))
+        logger.log('loss_fn: {}, loss: {:3.3f}'.format(loss_fn, loss))
+        return loss

Dev/brian_GLIF_model_setup.py
@@ -33,20 +33,21 @@ theta_s = theta_s - b_s * theta_s + delta_theta_s
 '''
 
 neurons = NeuronGroup(N=N, model=GLIF_eqs, threshold='v>30*mV', reset=reset, method='euler')
-# neurons.v = -65. * mV
-# neurons.theta_v = 1 * mV
-# neurons.theta_s = 30. * mV
-# neurons.E_L = -70. * mV
-# neurons.R_I = 18. * ohm
-# neurons.G = 0.8
-# neurons.C_m = 1.6
-# neurons.f_v = 0.14
+neurons.v = -80. * mV
+neurons.theta_v = 1 * mV
+neurons.theta_s = 30. * mV
+neurons.E_L = -80. * mV
+neurons.R_I = 10. * ohm
+neurons.G = 0.2
+neurons.C_m = 1.
+neurons.f_v = 0.04
 
 in_eqs = '''
-I_in : amp
+dI_in/dt = -I_in/tau : amp (clock-driven)
+I_ext_post = I_in : amp (summed)
 '''
 in_grp = SpikeGeneratorGroup(N, array([]), array([])*ms)  # placeholder arrays
-feedforward = Synapses(in_grp, neurons, model=in_eqs, on_pre='I_ext = I_in')
+feedforward = Synapses(in_grp, neurons, model=in_eqs, on_pre='I_in = 1 * mA')
 feedforward.connect(j='i')
 
 synapse_eqs = '''
@@ -59,7 +60,7 @@ synapses.connect()
 # w_mean = 0.3; w_var = 0.5
 # rand_ws = (w_mean - w_var) + 2 * w_var * np.random.random((N, N))
 # synapses.w = np.reshape(rand_ws, (-1,))
-# synapses.f_I = 0.4
-# print('S.w', synapses.w)
+synapses.f_I = 0.
+print('S.w', synapses.w)
 
-store()
+store('init')