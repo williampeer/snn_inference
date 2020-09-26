Constrain param intervals, add TargetModels, fix vr_dist normalisation

Dev/single_objective_network_optim.py
@@ -15,22 +15,22 @@ for optim in [ng.optimizers.DE]:
     for exp_i in range(num_exps):
         N = 12
         w_mean = 0.3; w_var = 0.5; rand_ws = (w_mean - w_var) + 2 * w_var * np.random.random((N ** 2))
-        instrum = ng.p.Instrumentation(rate=ng.p.Scalar(init=0.6).set_bounds(0.01, 1.),
+        instrum = ng.p.Instrumentation(rate=ng.p.Scalar(init=0.6).set_bounds(0.1, 1.),
                                        w=ng.p.Array(init=rand_ws).set_bounds(-1., 1.),
                                        f_I=ng.p.Array(init=0.4 * np.ones((N,))).set_bounds(0.01, 0.99),
                                        C_m=ng.p.Array(init=1.5 * np.ones((N,))).set_bounds(1., 3.),
                                        G=ng.p.Array(init=0.8 * np.ones((N,))).set_bounds(0.01, 0.99),
-                                       R_I=ng.p.Array(init=18. * np.ones((N,))).set_bounds(10., 30.),
+                                       R_I=ng.p.Array(init=18. * np.ones((N,))).set_bounds(12., 30.),
                                        f_v=ng.p.Array(init=0.14 * np.ones((N,))).set_bounds(0.01, 0.99),
                                        E_L=ng.p.Array(init=-65. * np.ones((N,))).set_bounds(-90., -30.),
 
                                        b_s=ng.p.Array(init=0.3 * np.ones((N,))).set_bounds(0.01, 0.9),
                                        b_v=ng.p.Array(init=0.5 * np.ones((N,))).set_bounds(0.01, 0.9),
                                        a_v=ng.p.Array(init=0.5 * np.ones((N,))).set_bounds(0.01, 0.9),
-                                       delta_theta_s=ng.p.Array(init=30. * np.ones((N,))).set_bounds(1., 40.),
-                                       delta_V=ng.p.Array(init=12. * np.ones((N,))).set_bounds(0.01, 35.),
-                                       theta_inf=ng.p.Array(init=-20. * np.ones((N,))).set_bounds(-25., 0.),
-                                       I_A=ng.p.Array(init=8. * np.ones((N,))).set_bounds(0.5, 10.),
+                                       delta_theta_s=ng.p.Array(init=10. * np.ones((N,))).set_bounds(6., 30.),
+                                       delta_V=ng.p.Array(init=6. * np.ones((N,))).set_bounds(0.01, 35.),
+                                       theta_inf=ng.p.Array(init=-15. * np.ones((N,))).set_bounds(-25., 0.),
+                                       I_A=ng.p.Array(init=2. * np.ones((N,))).set_bounds(0.5, 4.),
 
                                        loss_fn=ng.p.Choice(['van_rossum_dist', 'poisson_nll', 'gamma_factor']))
 
@@ -61,7 +61,7 @@ for optim in [ng.optimizers.DE]:
                                        exp_type='nevergrad',
                                        uuid='single_objective_optim',
                                        target_params=target_parameters,
-                                       param_names=list(fitted_params.keys())[:3],
+                                       param_names=list(fitted_params.keys())[2:],
                                        custom_title="KDEs for values across experiments ({})".format(optim.name),
                                        logger=logger, fname='single_objective_KDE_optim_{}'.format(optim.name))
 

Dev/spike_train_matlab_export.py
@@ -38,7 +38,7 @@ def main(argv):
 
     # model = torch.load(model_path)['model']
     # model = SleepModelWrappers.glif_sleep_model()
-    model = TargetModels.random_glif_model()
+    model = TargetModels.glif1()
     print('Loaded model.')
 
     interval_size = 4000

Models/GLIF.py
@@ -5,10 +5,10 @@ from torch import FloatTensor as FT
 
 class GLIF(nn.Module):
     parameter_names = ['w', 'E_L', 'C_m', 'G', 'R_I', 'f_v', 'f_I', 'delta_theta_s', 'b_s', 'a_v', 'b_v', 'theta_inf', 'delta_V', 'I_A']
-    parameter_intervals = {'w': [-1., 1.], 'E_L': [-90., -30.], 'C_m': [1., 2.], 'G': [0.01, 0.99], 'R_I': [12., 28.],
-                           'f_v': [0.01, 0.99], 'f_I': [0.01, 0.99], 'delta_theta_s': [1., 40.], 'b_s': [0.01, 0.9],
-                           'a_v': [0.01, 0.9], 'b_v': [0.01, 0.9], 'theta_inf': [-25., 0.], 'delta_V': [0.01, 35.],
-                           'I_A': [0.5, 10.]}
+    parameter_init_intervals = {'E_L': [-70., -37.], 'C_m': [1., 2.], 'G': [0.2, 0.9], 'R_I': [15., 24.],
+                           'f_v': [0.1, 0.5], 'f_I': [0.1, 0.7], 'delta_theta_s': [6., 16.], 'b_s': [0.1, 0.7],
+                           'a_v': [0.1, 0.7], 'b_v': [0.1, 0.5], 'theta_inf': [-25., 0.], 'delta_V': [6., 16.],
+                           'I_A': [0.5, 4.]}
 
     def __init__(self, device, N, parameters, C_m=1., G=0.7, R_I=18., E_L=-60., w_mean=0.2, w_var=0.4,
                  delta_theta_s=30., b_s=0.3, f_v=0.15, delta_V=12., f_I=0.3, I_A=1., b_v=0.5, a_v=0.5, theta_inf=-20.):
@@ -84,16 +84,16 @@ class GLIF(nn.Module):
         self.E_L.clamp(-90., -30.)
         self.C_m.clamp(1., 3.)
         self.G.clamp(0.01, 0.99)
-        self.R_I.clamp(15., 30.)
+        self.R_I.clamp(12., 30.)
         self.f_v.clamp(0.01, 0.99)
         self.f_I.clamp(0.01, 0.99)
-        self.delta_theta_s.clamp(1., 40.)
+        self.delta_theta_s.clamp(6., 30.)
         self.b_s.clamp(0.01, 0.9)
         self.a_v.clamp(0.01, 0.9)
         self.b_v.clamp(0.01, 0.9)
         self.theta_inf.clamp(-25., 0)
         self.delta_V.clamp(0.01, 35.)
-        self.I_A.clamp(0.5, 10.)
+        self.I_A.clamp(0.5, 4.)
         # self.I_A = FT(I_A)
         # self.delta_V = FT(delta_V)
 
@@ -112,7 +112,8 @@ class GLIF(nn.Module):
         self.I_additive = self.I_additive.clone().detach()
 
     def forward(self, x_in):
-        I = x_in + self.w.matmul(self.I_additive)
+        I = (x_in + self.w.matmul(self.I_additive))
+        # I = torch.relu(x_in + self.w.matmul(self.I_additive))
 
         dv = (I * self.R_I - self.G * (self.v - self.E_L)) / self.C_m
         v_next = self.v + dv

TargetModels/TargetModels.py
@@ -1,27 +1,64 @@
+import numpy as np
 import torch
-import torch.nn as nn
 from torch import tensor as T
 
 from Models.GLIF import GLIF
-from experiments import randomise_parameters
+from experiments import randomise_parameters, zip_dicts
 
 
-def glif_recurrent_net(N = 12):
-    glif_model = GLIF(device='cpu', parameters={}, N=N)
-    w = torch.ones(N,1) * T(N * [0.2])  # + 0.2 * I
-    glif_model.w = nn.Parameter(w, requires_grad=False)
-
-    return glif_model
-
-
-def random_glif_model(N = 12):
+def glif1(N = 12):
     torch.manual_seed(1234)
     free_parameters = {'w_mean': 0.2, 'w_var': 0.4, 'C_m': 1.35, 'G': 0.75, 'R_I': 18., 'E_L': -58., 'delta_theta_s': 25.,
                        'b_s': 0.4, 'f_v': 0.14, 'delta_V': 12., 'f_I': 0.4, 'I_A': 1., 'b_v': 0.5, 'a_v': 0.5, 'theta_inf': -25.}
     randomised_params = randomise_parameters(free_parameters, coeff=T(0.2))
+    print('randomised_params', randomised_params)
 
     return GLIF(device='cpu', parameters=randomised_params, N=N)
 
 
-# sut = random_glif_model()
-# print('sut R_I:', sut.state_dict())
+def glif2(N = 12):  # one silent neuron
+    torch.manual_seed(1234)
+    np.random.seed(1234)
+
+    free_parameters = {'w_mean': 0.1, 'w_var': 0.5, 'delta_theta_s': 25., 'b_s': 0.4, 'f_v': 0.3, 'delta_V': 12.,
+                       'b_v': 0.4, 'a_v': 0.4, 'theta_inf': -18., 'E_L': -57., 'C_m': 1.35, 'G': 0.7, 'I_A': 2.5, 'f_I': 0.3}
+    randomised_params = randomise_parameters(free_parameters, coeff=T(0.1), N_dim=N)
+    perturb_less_dict = {'R_I': 19.}
+    randomised_R_I = randomise_parameters(perturb_less_dict, coeff=T(0.02), N_dim=N)
+    randomised_params = zip_dicts(randomised_params, randomised_R_I)
+    print('randomised_params', randomised_params)
+
+    return GLIF(device='cpu', parameters=randomised_params, N=N)
+
+
+def glif3(N = 12):
+    torch.manual_seed(1234)
+    np.random.seed(1234)
+    free_parameters = {'w_mean': 0.13, 'w_var': 0.33, 'C_m': 1.14, 'G': 0.82, 'R_I': 24., 'E_L': -67., 'delta_theta_s': 15.,
+                       'b_s': 0.4, 'f_v': 0.3, 'delta_V': 10., 'f_I': 0.25, 'I_A': 1.5, 'b_v': 0.4, 'a_v': 0.35, 'theta_inf': -15.}
+    randomised_params = randomise_parameters(free_parameters, coeff=T(0.1), N_dim=N)
+    print('randomised_params', randomised_params)
+
+    return GLIF(device='cpu', parameters=randomised_params, N=N)
+
+
+def glif_slower_rate_async(N = 12):  # some silent neurons.
+    torch.manual_seed(1234)
+    np.random.seed(1234)
+    free_parameters = {'w_mean': 0.3, 'w_var': 0.5, 'C_m': 1.5, 'G': 0.7, 'R_I': 18., 'E_L': -65., 'delta_theta_s': 12.,
+                       'b_s': 0.4, 'f_v': 0.3, 'delta_V': 10., 'f_I': 0.25, 'I_A': 1.7, 'b_v': 0.4, 'a_v': 0.35, 'theta_inf': -20.}
+    randomised_params = randomise_parameters(free_parameters, coeff=T(0.1), N_dim=N)
+    print('randomised_params', randomised_params)
+
+    return GLIF(device='cpu', parameters=randomised_params, N=N)
+
+
+def glif_slower_more_synchronous(N = 12):
+    torch.manual_seed(1234)
+    np.random.seed(1234)
+    free_parameters = {'w_mean': 0.2, 'w_var': 0.1, 'C_m': 2.7, 'G': 0.75, 'R_I': 15., 'E_L': -43., 'delta_theta_s': 27.,
+                       'b_s': 0.2, 'f_v': 0.2, 'delta_V': 8., 'f_I': 0.6, 'I_A': 1.4, 'b_v': 0.1, 'a_v': 0.1, 'theta_inf': -26.}
+    randomised_params = randomise_parameters(free_parameters, coeff=T(0.02), N_dim=N)
+    print('randomised_params', randomised_params)
+
+    return GLIF(device='cpu', parameters=randomised_params, N=N)

experiments.py
@@ -15,12 +15,16 @@ def draw_from_uniform(parameters, parameter_intervals, N):
     return new_dict
 
 
-def randomise_parameters(initial_parameters, coeff=torch.tensor(0.5)):
+def randomise_parameters(initial_parameters, coeff=torch.tensor(0.5), N_dim=None):
     res = initial_parameters.copy()
     for key in initial_parameters.keys():
         # rand_sign = round(torch.randn())*2 -1
-        rand_sign = 2*torch.randint(0, 1, (1,))[0] -1
-        res[key] = res[key] + rand_sign * coeff * torch.randn((1,))[0] * res[key]
+        if N_dim is not None:
+            rand_sign = 2 * torch.randint(0, 1, (1,))[0] - 1
+            res[key] = res[key] + rand_sign * coeff * torch.randn((int(N_dim),)) * res[key]
+        else:
+            rand_sign = 2 * torch.randint(0, 1, (1,))[0] - 1
+            res[key] = res[key] + rand_sign * coeff * torch.randn((1,))[0] * res[key]
 
     return res
 

spike_metrics.py
@@ -24,7 +24,7 @@ def van_rossum_dist(spikes, target_spikes, tau):
 
 def euclid_dist(spikes1, spikes2):
     # sqrt((s1 - s2) ** 2)
-    return torch.sqrt(torch.pow(torch.sub(spikes1, spikes2), 2).sum() + 1e-18) / (spikes1.shape[0] * spikes1.shape[1])
+    return torch.sqrt(torch.pow(torch.sub(spikes1, spikes2), 2).sum() + 1e-18) / (spikes1.shape[1])
 
 
 def mse(s1, s2):