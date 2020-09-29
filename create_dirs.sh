6ea4c76533ddf532a520fe90f5f47f71863e8acc
William <williapb@gmail.com>
2020-09-29 09:18:08 +0100

    Refactor

--- create_dirs.sh
+++ create_dirs.sh
@@ -9,6 +9,8 @@ mkdir figures/DataDriven
 mkdir figures/Synthetic
 mkdir figures/SanityCheck
 mkdir figures/RetrieveFitted
+mkdir figures/single_objective_optim
+mkdir figures/multiobjective_optim
 
 mkdir saved/plot_data
 
@@ -17,6 +19,8 @@ mkdir Test/figures
 mkdir Test/Logs
 
 mkdir Test/figures/default
+mkdir Test/figures/single_objective_optim
+mkdir Test/figures/multiobjective_optim
 mkdir Test/saved/plot_data
 
 mkdir ~/data/sleep_data/matlab_export/
