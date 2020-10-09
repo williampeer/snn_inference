Implement TargetEnsembleModels - glif takes seed, fix vrdfrd for silent targets, refactor

Models/GLIF.py
@@ -6,12 +6,12 @@ from torch import FloatTensor as FT
 class GLIF(nn.Module):
     parameter_names = ['w', 'E_L', 'C_m', 'G', 'R_I', 'f_v', 'f_I', 'delta_theta_s', 'b_s', 'a_v', 'b_v', 'theta_inf', 'delta_V', 'I_A']
     parameter_init_intervals = {'E_L': [-70., -37.], 'C_m': [1.2, 1.8], 'G': [0.2, 0.9], 'R_I': [100., 110.],
-                                'f_v': [0.2, 0.4], 'f_I': [0.1, 0.4], 'delta_theta_s': [6., 15.], 'b_s': [0.2, 0.4],
+                                'f_v': [0.2, 0.4], 'f_I': [0.1, 0.4], 'delta_theta_s': [5., 30.], 'b_s': [0.2, 0.4],
                                 'a_v': [0.2, 0.5], 'b_v': [0.1, 0.5], 'theta_inf': [-20., -10.], 'delta_V': [6., 16.],
                                 'I_A': [0.5, 2.]}
 
     def __init__(self, device, N, parameters, C_m=1., G=0.7, R_I=18., E_L=-60., w_mean=0.2, w_var=0.4,
-                 delta_theta_s=30., b_s=0.3, f_v=0.15, delta_V=12., f_I=0.3, I_A=1., b_v=0.5, a_v=0.5, theta_inf=-20.):
+                 delta_theta_s=15., b_s=0.3, f_v=0.15, delta_V=12., f_I=0.3, I_A=1., b_v=0.5, a_v=0.5, theta_inf=-20.):
         super(GLIF, self).__init__()
         # self.device = device
 
@@ -35,6 +35,8 @@ class GLIF(nn.Module):
                     delta_V = FT(torch.ones((N,)) * parameters[key])
                 elif key == 'f_I':
                     f_I = FT(torch.ones((N,)) * parameters[key])
+                elif key == 'I_A':
+                    I_A = FT(torch.ones((N,)) * parameters[key])
                 elif key == 'b_v':
                     b_v = FT(torch.ones((N,)) * parameters[key])
                 elif key == 'a_v':
@@ -45,8 +47,6 @@ class GLIF(nn.Module):
                     w_mean = FT(torch.ones((N,)) * parameters[key])
                 elif key == 'w_var':
                     w_var = FT(torch.ones((N,)) * parameters[key])
-                elif key == 'I_A':
-                    I_A = FT(torch.ones((N,)) * parameters[key])
 
         __constants__ = ['N', 'E_L', 'delta_theta_s', 'b_s', 'a_v', 'b_v', 'theta_inf']
         self.N = N
@@ -65,7 +65,13 @@ class GLIF(nn.Module):
         self.theta_v = torch.ones((self.N,))
         self.I_additive = torch.zeros((self.N,))
 
-        rand_ws = (w_mean - w_var) + 2 * w_var * torch.rand((self.N, self.N))
+        if parameters.__contains__('preset_weights'):
+            # print('DEBUG: Setting w to preset weights: {}'.format(parameters['preset_weights']))
+            # print('Setting w to preset weights.')
+            rand_ws = parameters['preset_weights']
+            assert rand_ws.shape[0] == N and rand_ws.shape[1] == N, "shape of weights matrix should be NxN"
+        else:
+            rand_ws = (w_mean - w_var) + 2 * w_var * torch.rand((self.N, self.N))
         self.w = nn.Parameter(FT(rand_ws), requires_grad=True)
         self.E_L = nn.Parameter(FT(E_L), requires_grad=True)
         self.C_m = nn.Parameter(FT(C_m), requires_grad=True)

TargetModels/TargetModels.py
@@ -7,11 +7,23 @@ from experiments import randomise_parameters, zip_dicts
 
 
 def glif1(N = 12):
-    torch.manual_seed(1234)
-    free_parameters = {'w_mean': 0.2, 'w_var': 0.4, 'C_m': 1.35, 'G': 0.75, 'R_I': 110., 'E_L': -58., 'delta_theta_s': 25.,
-                       'b_s': 0.4, 'f_v': 0.14, 'delta_V': 12., 'f_I': 0.4, 'I_A': 1., 'b_v': 0.5, 'a_v': 0.5, 'theta_inf': -25.}
-    randomised_params = randomise_parameters(free_parameters, coeff=T(0.2))
+    torch.manual_seed(1)
+    np.random.seed(1)
+    free_parameters = {'C_m': 1.3, 'G': 0.8, 'E_L': -56., 'delta_theta_s': 25.,
+                       'b_s': 0.4, 'f_v': 0.14, 'delta_V': 12., 'f_I': 0.4, 'I_A': 1.4, 'b_v': 0.5, 'a_v': 0.5, 'theta_inf': -27.}
+    randomised_params = randomise_parameters(free_parameters, coeff=T(0.05), N_dim=N)
     # print('randomised_params', randomised_params)
+
+    return GLIF(device='cpu', parameters=zip_dicts(randomised_params, {'w_mean': 0.4, 'w_var': 0.6, 'R_I': 130.}), N=N)
+
+
+def glif1_2(N = 12):
+    torch.manual_seed(4321)
+    np.random.seed(4321)
+    free_parameters = {'w_mean': 0.34, 'w_var': 0.5, 'C_m': 1.5, 'G': 0.75, 'R_I': 100., 'E_L': -62., 'delta_theta_s': 12.,
+                       'b_s': 0.4, 'f_v': 0.14, 'delta_V': 6., 'f_I': 0.3, 'I_A': 1.4, 'b_v': 0.5, 'a_v': 0.5, 'theta_inf': -26.}
+    randomised_params = randomise_parameters(free_parameters, coeff=T(0.2), N_dim=N)
+    print('randomised_params', randomised_params)
 
     return GLIF(device='cpu', parameters=randomised_params, N=N)
 
@@ -20,7 +32,7 @@ def glif2(N = 12):
     torch.manual_seed(1234)
     np.random.seed(1234)
 
-    free_parameters = {'w_mean': 0.1, 'w_var': 0.5, 'delta_theta_s': 25., 'b_s': 0.4, 'f_v': 0.3, 'delta_V': 12.,
+    free_parameters = {'w_mean': 0.3, 'w_var': 0.5, 'delta_theta_s': 25., 'b_s': 0.4, 'f_v': 0.3, 'delta_V': 12.,
                        'b_v': 0.4, 'a_v': 0.4, 'theta_inf': -18., 'E_L': -57., 'C_m': 1.35, 'G': 0.7, 'I_A': 2.5, 'f_I': 0.3}
     randomised_params = randomise_parameters(free_parameters, coeff=T(0.1), N_dim=N)
     perturb_less_dict = {'R_I': 120.}
@@ -57,7 +69,7 @@ def glif_async(N = 12):
 def glif_slower_more_synchronous(N = 12):
     torch.manual_seed(1234)
     np.random.seed(1234)
-    free_parameters = {'w_mean': 0.2, 'w_var': 0.1, 'C_m': 2.7, 'G': 0.75, 'R_I': 80., 'E_L': -43., 'delta_theta_s': 27.,
+    free_parameters = {'w_mean': 0.2, 'w_var': 0.1, 'C_m': 2.7, 'G': 0.75, 'R_I': 100., 'E_L': -43., 'delta_theta_s': 27.,
                        'b_s': 0.2, 'f_v': 0.2, 'delta_V': 8., 'f_I': 0.6, 'I_A': 1.4, 'b_v': 0.1, 'a_v': 0.1, 'theta_inf': -26.}
     randomised_params = randomise_parameters(free_parameters, coeff=T(0.02), N_dim=N)
     print('randomised_params', randomised_params)

Test/test_GLIF_SNN.py
@@ -2,32 +2,34 @@ import torch
 
 import model_util
 import spike_metrics
-from TargetModels import TargetModels
+from TargetModels import TargetModels, TargetEnsembleModels
 from experiments import poisson_input
 from plot import plot_neuron, plot_spiketrains_side_by_side
 
-# static_parameters = {'N': 10}
-# free_parameters = {'w_mean': 0.2, 'w_var': 0.3}
-# snn = GLIF(device='cpu', parameters=zip_dicts(static_parameters, free_parameters))
-snn = TargetModels.glif1(N = 12); ext_name = '1'
-# snn = TargetModels.glif2(N = 12); ext_name = '2'
-# snn = TargetModels.glif3(N = 12); ext_name = '3'
-# snn = TargetModels.glif_async(N = 12); ext_name = 'glif_async'
-# snn = TargetModels.glif_slower_more_synchronous(N = 12); ext_name = 'glif_slower_more_synchronous'
-# snn = TargetModels.glif_larger(); ext_name = 'glif_larger'
+for random_seed in range(1, 6):
+    # static_parameters = {'N': 10}
+    # free_parameters = {'w_mean': 0.2, 'w_var': 0.3}
+    # snn = GLIF(device='cpu', parameters=zip_dicts(static_parameters, free_parameters))
+    snn = TargetEnsembleModels.glif_ensembles_model(random_seed=random_seed, N = 12); ext_name = 'ensembles_1'
+    # snn = TargetModels.glif1(N = 12); ext_name = '1'
+    # snn = TargetModels.glif1_2(N = 12); ext_name = '1_2'
+    # snn = TargetModels.glif2(N = 12); ext_name = '2'
+    # snn = TargetModels.glif3(N = 12); ext_name = '3'
+    # snn = TargetModels.glif_async(N = 12); ext_name = 'glif_async'
+    # snn = TargetModels.glif_slower_more_synchronous(N = 12); ext_name = 'glif_slower_more_synchronous'
 
-inputs = poisson_input(10., t=4000, N=snn.N)  # now assumes rate in Hz
-print('#inputs: {}'.format(inputs.sum()))
-membrane_potentials, spikes = model_util.feed_inputs_sequentially_return_spikes_and_potentials(snn, inputs)
-print('#spikes: {}'.format(spikes.sum()))
-plot_neuron(membrane_potentials.data, title='GLIF neuron plot ({:.2f} spikes)'.format(spikes.sum()), fname_ext='test_GLIF_poisson_input' + '_' + ext_name)
+    inputs = poisson_input(10., t=4000, N=snn.N)  # now assumes rate in Hz
+    print('#inputs: {}'.format(inputs.sum()))
+    membrane_potentials, spikes = model_util.feed_inputs_sequentially_return_spikes_and_potentials(snn, inputs)
+    print('#spikes: {}'.format(spikes.sum()))
+    plot_neuron(membrane_potentials.data, title='GLIF neuron plot ({:.2f} spikes)'.format(spikes.sum()), fname_ext='test_GLIF_poisson_input' + '_' + ext_name)
 
-zeros = torch.zeros_like(inputs)
-membrane_potentials_zeros, spikes_zeros = model_util.feed_inputs_sequentially_return_spikes_and_potentials(snn, zeros)
-plot_neuron(membrane_potentials_zeros.data, title='Neuron plot ({:.2f} spikes)'.format(spikes_zeros.sum()), fname_ext='test_GLIF_no_input'  + '_' + ext_name)
+    zeros = torch.zeros_like(inputs)
+    membrane_potentials_zeros, spikes_zeros = model_util.feed_inputs_sequentially_return_spikes_and_potentials(snn, zeros)
+    plot_neuron(membrane_potentials_zeros.data, title='Neuron plot ({:.2f} spikes)'.format(spikes_zeros.sum()), fname_ext='test_GLIF_no_input'  + '_' + ext_name)
 
-plot_spiketrains_side_by_side(spikes, spikes_zeros, 'test_GLIF', title='Test GLIF spiketrains random and zero input', legend=['Poisson input', 'No input'])
+    plot_spiketrains_side_by_side(spikes, spikes_zeros, 'test_GLIF', title='Test GLIF spiketrains random and zero input', legend=['Poisson input', 'No input'])
 
-tau_vr = torch.tensor(4.0)
-loss = spike_metrics.van_rossum_dist(spikes, spikes_zeros, tau=tau_vr)
-print('tau_vr: {}, loss: {}'.format(tau_vr, loss))
+    tau_vr = torch.tensor(4.0)
+    loss = spike_metrics.van_rossum_dist(spikes, spikes_zeros, tau=tau_vr)
+    print('tau_vr: {}, loss: {}'.format(tau_vr, loss))

spike_metrics.py
@@ -40,6 +40,7 @@ def van_rossum_squared_distance(s1, s2, tau):
 def firing_rate_distance(model_spikes, target_spikes):
     mean_model_rate = model_spikes.sum(axis=0)
     mean_targets_rate = target_spikes.sum(axis=0)
-    silent_penalty = 2.*torch.exp(-mean_model_rate).sum() / model_spikes.shape[1]
+    # f_penalty(x,y) = sqrt(pow(2e^-x - 2e^-y))
+    silent_penalty = torch.sqrt(torch.pow(2.*torch.exp(-mean_model_rate) - 2.*torch.exp(-mean_targets_rate), 2).sum()) / model_spikes.shape[1]
     return torch.sqrt(torch.pow(torch.sub(mean_model_rate, mean_targets_rate), 2).sum() + 1e-18)/(model_spikes.shape[0]) \
            + silent_penalty