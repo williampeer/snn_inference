219d7c6ff9ec4cb0937bc0276ee64ce15d55c964
William <williapb@gmail.com>
2020-12-14 13:45:10 +0100

    Add tau_vr to fname losses

--- fit_to_model_exp_suite.py
+++ fit_to_model_exp_suite.py
@@ -26,7 +26,7 @@ def stats_training_iterations(model_parameters, model, poisson_rate, train_losse
                                                  logger=logger)
         plot_losses(training_loss=train_losses, test_loss=test_losses, uuid=constants.UUID, exp_type=exp_type_str,
                     custom_title='Loss ({}, {}, lr={})'.format(model.__class__.__name__, constants.optimiser.__name__, constants.learn_rate),
-                    fname='training_and_test_loss_exp_{}_loss_fn_{}'.format(exp_num, constants.loss_fn))
+                    fname='training_and_test_loss_exp_{}_loss_fn_{}_tau_vr_{}'.format(exp_num, constants.loss_fn, constants.tau_van_rossum))
 
     logger.log('train_losses: #{}'.format(train_losses))
     mean_test_loss = torch.mean(torch.tensor(test_losses)).clone().detach().numpy()
