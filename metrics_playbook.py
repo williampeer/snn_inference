2f5d3250601917391175d7df270babfdb2b510b2
William <williapb@gmail.com>
2021-01-25 19:38:33 +0100

    Pseudocode metrics

---
+++ metrics_playbook.py
@@ -0,0 +1,26 @@
+
+
+# import saved model
+
+# import target model
+
+# calc euclid dist all weights || per pop./ensemble
+
+# ----------
+
+# per exp.:
+# import losses
+# modify plot_losses fn to be for averages and stds
+
+
+# ----------
+
+# iff model saved after train iter. #0:
+#   calc. dist at #0 and #-1 to target. plot/report relative improvement (%?)
+
+
+# ---------
+
+# geodesic(?) distance NMF modules
+
+# ACs in appendix?
