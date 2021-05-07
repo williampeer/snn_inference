66ff32c876b43438c960a83578438e9a275359ed
William <williapb@gmail.com>
2021-05-07 15:37:53 +0200

    Try readout as s * (tau+1)/2

--- Models/LIF_R_ASC.py
+++ Models/LIF_R_ASC.py
@@ -132,7 +132,8 @@ class LIF_R_ASC(nn.Module):
         self.I_additive = self.I_additive - self.f_I * self.I_additive + spiked * self.f_I
 
         # return self.v, self.s * self.tau_s
-        return self.s * self.tau_s  # use synaptic current as spike signal
+        # return self.s * self.tau_s  # use synaptic current as spike signal
+        return self.s * (self.tau_s + 1) / 2.  # return readout of synaptic current as spike signal
 
         # return self.v, self.spiked
         # return self.spiked
