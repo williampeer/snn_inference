88b59e61328a2956dd58959781405c4559829888
William <williapb@gmail.com>
2021-06-02 12:08:05 +0200

    Stage frd, vrd, pcc, rph, kl_div

--- main.py
+++ main.py
@@ -140,10 +140,10 @@ def main(argv):
 
     if loss_fn is None:
         loss_functions = [LossFn.FIRING_RATE_DIST.name,
-                          # LossFn.VAN_ROSSUM_DIST.name,
+                          LossFn.VAN_ROSSUM_DIST.name,
                           LossFn.PEARSON_CORRELATION_COEFFICIENT.name,
                           # LossFn.FANO_FACTOR_DIST.name,
-                          LossFn.RATE_FANO_HYBRID.name,
+                          # LossFn.RATE_FANO_HYBRID.name,
                           LossFn.RATE_PCC_HYBRID.name,
                           # LossFn.CV_DIST.name]
                           LossFn.KL_DIV.name]
