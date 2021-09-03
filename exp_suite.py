Fix nan-bug in grad backprop

exp_suite.py
@@ -157,38 +157,32 @@ def fit_model(logger, constants, model_class, params_model, exp_num, target_mode
             if constants.EXP_TYPE == ExperimentType.SanityCheck:
                 train_input = gen_train_input
 
-        try:
-            avg_unseen_loss, abs_grads_mean, last_loss = fit_batches(model, gen_inputs=train_input, target_spiketrain=train_targets,
-                                                                     poisson_input_rate=poisson_input_rate, optimiser=optim,
-                                                                     constants=constants, train_i=train_i, logger=logger)
+        # try:
+            avg_unseen_loss, abs_grads_mean, last_loss, converged = fit_batches(model, gen_inputs=train_input, target_spiketrain=train_targets,
+                                                                                poisson_input_rate=poisson_input_rate, optimiser=optim,
+                                                                                constants=constants, train_i=train_i, logger=logger)
+
+            cur_params = model.state_dict()
+            logger.log('current parameters {}'.format(cur_params))
+
+            for p_i, key in enumerate(cur_params):
+                parameters[p_i].append(cur_params[key].clone().detach().numpy())
+
             # if constants.EXP_TYPE is not ExperimentType.DataDriven:
             release_computational_graph(target_model, constants.initial_poisson_rate)
 
             logger.log(parameters=[avg_unseen_loss, abs_grads_mean])
             test_losses = np.concatenate((test_losses, np.asarray([avg_unseen_loss])))
-
-            cur_params = model.state_dict()
-            logger.log('current parameters {}'.format(cur_params))
-            for p_i, key in enumerate(cur_params):
-                parameters[p_i].append(cur_params[key].clone().detach().numpy())
         # parameters[p_i + 1].append(poisson_input_rate.clone().detach().numpy())
         # poisson_rates.append(poisson_input_rate.clone().detach().numpy())
 
         # max_grads_mean = np.max((max_grads_mean, abs_grads_mean))
         # converged = abs(abs_grads_mean) <= 0.1 * abs(max_grads_mean)  # and validation_loss < np.max(validation_losses)
-        except Exception as e:
-            logger.log('======== Exception in fit ===========\n{}'.format(e))
-            if error_logger is not None:
-                error_logger.log('Exception occurred: {}'.format(e))
-            # converged = True  # stop if vanishing gradients
-
-        current_model_parameters = model.get_parameters()
-        param_grads_converged = []
-        for p_i in range(len(current_model_parameters)):
-            cur_p_mean_grad = np.mean(current_model_parameters[p_i].grad.numpy())
-            cur_converged = cur_p_mean_grad < 1e-03 * np.mean(current_model_parameters[p_i].numpy())
-            param_grads_converged.append(cur_converged)
-        converged = np.array(param_grads_converged).sum() == len(param_grads_converged)
+        # except Exception as e:
+        #     logger.log('======== Exception in fit ===========\n{}'.format(e))
+        #     if error_logger is not None:
+        #         error_logger.log('Exception occurred: {}'.format(e))
+        #     # converged = True  # stop if vanishing gradients
 
         train_loss = evaluate_loss(model, inputs=train_input, p_rate=poisson_input_rate.clone().detach(),
                                    target_spiketrain=train_targets, label='train i: {}'.format(train_i),
@@ -241,29 +235,29 @@ def run_exp_loop(logger, constants, model_class, target_model=None, error_logger
 
         init_params_model = draw_from_uniform(model_class.parameter_init_intervals, num_neurons)
 
-        try:
-            recovered_parameters, train_losses, test_losses, train_i, poisson_rates = \
-                fit_model(logger, constants, model_class, init_params_model, exp_num=exp_i, target_model=target_model,
-                          target_parameters=target_parameters, num_neurons=num_neurons,
-                          error_logger=error_logger)
+        # try:
+        recovered_parameters, train_losses, test_losses, train_i, poisson_rates = \
+            fit_model(logger, constants, model_class, init_params_model, exp_num=exp_i, target_model=target_model,
+                      target_parameters=target_parameters, num_neurons=num_neurons,
+                      error_logger=error_logger)
 
-            # logger.log('poisson rates for exp {}'.format(exp_i), poisson_rates)
+        # logger.log('poisson rates for exp {}'.format(exp_i), poisson_rates)
 
-            if train_i >= constants.train_iters:
-                print('DID NOT CONVERGE FOR SEED, CONTINUING ON TO NEXT SEED. exp_i: {}, train_i: {}, train_losses: {}, test_losses: {}'
-                      .format(exp_i, train_i, train_losses, test_losses))
+        if train_i >= constants.train_iters:
+            print('DID NOT CONVERGE FOR SEED, CONTINUING ON TO NEXT SEED. exp_i: {}, train_i: {}, train_losses: {}, test_losses: {}'
+                  .format(exp_i, train_i, train_losses, test_losses))
 
-            for p_i, key in enumerate(recovered_parameters):
-                if exp_i == constants.start_seed:
-                    recovered_param_per_exp[key] = [recovered_parameters[key]]
-                else:
-                    recovered_param_per_exp[key].append(recovered_parameters[key])
+        for p_i, key in enumerate(recovered_parameters):
+            if exp_i == constants.start_seed:
+                recovered_param_per_exp[key] = [recovered_parameters[key]]
+            else:
+                recovered_param_per_exp[key].append(recovered_parameters[key])
             # if poisson_rates is not None and len(poisson_rates) > 0:
             #     poisson_rate_per_exp.append(poisson_rates[-1])
-        except Exception as e:
-            logger.log('======== ERROR ===========\nException occurred: {}'.format(e))
-            error_logger.log('Exception occurred: {}'.format(e))
-            print(e)
+        # except Exception as e:
+        #     logger.log('======== ERROR ===========\nException occurred: {}'.format(e))
+        #     error_logger.log('Exception occurred: {}'.format(e))
+        #     print(e)
 
             # TODO: Consider fixing
             # stats_training_iterations(model_parameters=parameters, model=model, poisson_rate=poisson_input_rate,

fit.py
@@ -23,6 +23,7 @@ def fit_batches(model, gen_inputs, target_spiketrain, poisson_input_rate, optimi
         avg_abs_grads.append([])
 
     optimiser.zero_grad()
+    converged_batches = []
     # poisson_input_rate.grad = torch.tensor(0.)
     for batch_i in range(batch_N):
         print('batch #{}'.format(batch_i))
@@ -49,6 +50,7 @@ def fit_batches(model, gen_inputs, target_spiketrain, poisson_input_rate, optimi
         # loss.backward()
 
         # poisson_input_rate.grad = torch.mean(current_inputs.grad)  # TODO: test w. "final" learn rate
+        param_grads_converged = []
         for p_i, param in enumerate(list(model.parameters())):
             logger.log('grad for param #{}: {}'.format(p_i, param.grad))
             if constants.norm_grad_flag is True:
@@ -56,10 +58,14 @@ def fit_batches(model, gen_inputs, target_spiketrain, poisson_input_rate, optimi
                 if max_grad > 0:
                     param.grad = param.grad/torch.max(param.grad)  # normalise
 
-        # print('list(model.parameters())', list(model.parameters()))
-        for p_i, param in enumerate(list(model.parameters())):
-            # print('p_i, param.grad', p_i, param.grad)
             avg_abs_grads[p_i].append(np.mean(np.abs(param.grad.clone().detach().numpy())))
+
+            cur_p_mean_grad = np.mean(param.grad.clone().detach().numpy())
+            cur_converged = cur_p_mean_grad < 1e-02 * np.mean(param.clone().detach().numpy())
+            param_grads_converged.append(cur_converged)
+
+        converged = np.array(param_grads_converged).sum() == len(param_grads_converged)
+        converged_batches.append(converged)
 
         if constants.EXP_TYPE is not ExperimentType.SanityCheck:
             avg_abs_grads[p_i + 1].append(np.abs(poisson_input_rate.grad.clone().detach().numpy()))
@@ -82,4 +88,6 @@ def fit_batches(model, gen_inputs, target_spiketrain, poisson_input_rate, optimi
     logger.log('train_i #: {},\navg_abs_grads: {}'.format(train_i, avg_abs_grads))
     gen_inputs = None
 
-    return avg_batch_loss, np.mean(np.asarray(avg_abs_grads, dtype=np.float)), batch_losses[-1]
+    converged = np.array(converged_batches).sum() == len(converged_batches)
+
+    return avg_batch_loss, np.mean(np.asarray(avg_abs_grads, dtype=np.float)), batch_losses[-1], converged

spike_metrics.py
@@ -45,7 +45,9 @@ def van_rossum_dist_one_to_K(spikes, target_spikes, tau):
     c1 = torch_van_rossum_convolution(spikes=spikes.reshape((-1, 1)), tau=tau)
     c1 = torch.ones((1, target_spikes.shape[1])) * c1.reshape((-1, 1))  # broadcast
     c2 = torch_van_rossum_convolution(spikes=target_spikes, tau=tau)
-    euclid_per_node = torch.sqrt(torch.pow(torch.sub(c1, c2), 2) + 1e-12).sum(dim=0)
+    pow_res = torch.pow(torch.sub(c1, c2), 2)
+    # pow_res[torch.isnan(pow_res)] = 1e-06
+    euclid_per_node = torch.sqrt(pow_res + 1e-12).sum(dim=0)
     return euclid_per_node
 
 
@@ -69,7 +71,9 @@ def greedy_shortest_dist_vr(spikes, target_spikes, tau):
 
 def euclid_dist(vec1, vec2):
     # sqrt((s1 - s2) ** 2)
-    return torch.sqrt(torch.pow(torch.sub(vec2, vec1), 2).sum() + 1e-12)
+    pow_res = torch.pow(torch.sub(vec2, vec1), 2)
+    # pow_res[torch.isnan(pow_res)] = 1e-06
+    return torch.sqrt(pow_res.sum() + 1e-12)
 
 
 def mse(s1, s2):
@@ -154,7 +158,21 @@ def calc_pearsonr(counts_out, counts_tar):
     mu_tar = torch.mean(counts_tar, dim=0)
     std_tar = torch.std(counts_tar, dim=0)  # * counts_out.shape[0]  # Bessel correction correction
 
+    # std_out[torch.isnan(std_out)] = 1.
+    # std_tar[torch.isnan(std_tar)] = 1.
+    std_out[std_out == 0] = 1.
+    std_tar[std_tar == 0] = 1.
+
     pcorrcoeff = (counts_out - torch.ones_like(counts_out) * mu_out) * (counts_tar - torch.ones_like(counts_tar) * mu_tar) / (std_out * std_tar)
+
+    # print('..............-------------------................')
+    # print('counts_out: {}'.format(counts_out))
+    # print('counts_tar: {}'.format(counts_tar))
+    # print('pcorrcoeff: {}'.format(pcorrcoeff))
+    # print('..............-------------------................')
+
+    assert torch.isnan(pcorrcoeff).sum() == 0, "found nan-values in pcorrcoeff: {}".format(pcorrcoeff)
+
     return pcorrcoeff
 
 
@@ -170,7 +188,15 @@ def correlation_metric_distance(out, tar, bins=NUM_BINS):
     # pcorrcoeff = audtorch.metrics.functional.pearsonr(tar_counts, out_counts)
     pcorrcoeff = calc_pearsonr(tar_counts, out_counts)
     neg_dist = torch.ones_like(pcorrcoeff) - pcorrcoeff  # max 0.
-    return torch.sqrt(torch.pow(neg_dist, 2) + 1e-12).sum() / out.shape[0]
+    squared_dist = torch.pow(neg_dist, 2)
+    # squared_dist[torch.isnan(squared_dist)] = 1e-06
+    dist = torch.sqrt(squared_dist + 1e-12).sum() / out.shape[0]
+    # print('-----------************------------***********------------')
+    # print('neg_dist: {}'.format(neg_dist))
+    # print('squared_dist: {}'.format(squared_dist))
+    # print('dist: {}'.format(dist))
+    # print('-----------************------------***********------------')
+    return dist
 
 
 def CV_dist(out, tar, bins=NUM_BINS):
@@ -199,7 +225,8 @@ def shortest_dist_rates(spikes, target_spikes):
     target_rates = target_spikes.sum(axis=0) * 1000. / target_spikes.shape[0]
     target_rates, _ = torch.sort(target_rates)
 
-    return torch.sqrt(torch.pow(torch.sub(spike_rates, target_rates), 2).sum() + 1e-12)
+    # return torch.sqrt(torch.pow(torch.sub(spike_rates, target_rates), 2).sum() + 1e-12)
+    return euclid_dist(spike_rates, target_rates)
 
 
 def shortest_dist_rates_w_silent_penalty(spikes, target_spikes):
@@ -210,6 +237,10 @@ def shortest_dist_rates_w_silent_penalty(spikes, target_spikes):
     target_rates = target_spikes.sum(dim=0) * 1000. / target_spikes.shape[0]
     target_rates, _ = torch.sort(target_rates)
 
-    silent_penalty = torch.sqrt(torch.pow(torch.exp(-spike_rates) - torch.exp(target_rates), 2).sum() + 1e-12)
-    return torch.sqrt(torch.pow(torch.sub(spike_rates, target_rates), 2).sum() + 1e-12) + silent_penalty
+    pow_res = torch.pow(torch.exp(-spike_rates) - torch.exp(target_rates), 2)
+    # pow_res[torch.isnan(pow_res)] = 1e-06
+
+    silent_penalty = torch.sqrt(pow_res.sum() + 1e-12)
+    # return torch.sqrt(torch.pow(torch.sub(spike_rates, target_rates), 2).sum() + 1e-12) + silent_penalty
+    return euclid_dist(spike_rates, target_rates) + silent_penalty
 