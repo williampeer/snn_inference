df8a3b7cb2e497f8dba93f46a7682b8777424405
William <williapb@gmail.com>
2021-12-23 18:19:46 +0100

    (WIP) debug corr & RMSE (torch impl)

--- analysis/sbi_performance_measures.py
+++ analysis/sbi_performance_measures.py
@@ -12,18 +12,18 @@ from analysis.spike_train_matlab_export import simulate_and_save_model_spike_tra
 
 
 def OU_process_input(t=10000):
-    mu = np.array([1., 1., 0, 1.])
-    tau = np.array([2., 2., 1., 2.])
-    q = np.array([0.5, 0.5, 0, 0.5])
-    dW = np.random.normal()
+    mu = torch.tensor([1., 1., 0, 1.])
+    tau = torch.tensor([2., 2., 1., 2.])
+    q = torch.tensor([0.5, 0.5, 0, 0.5])
+    dW = torch.randn((t, 4))
 
-    I_0 = np.array([0.5, 0.5, 0, 0.5])
-    I_interval = np.asarray(I_0)
-    I = np.asarray(I_0)
-    for t_i in range(t):
-        dI = (I - mu)/tau + np.sqrt(2/tau) * q * dW
+    I_0 = torch.tensor([0.5, 0.5, 0, 0.5])
+    I_interval = I_0.clone().detach()
+    I = I_0.clone().detach()
+    for t_i in range(t-1):
+        dI = (I - mu)/tau + torch.sqrt(2/tau) * q * dW[t_i, :]
         I = I + dI
-        I_interval = np.concatenate((I_interval, dI))
+        I_interval = torch.vstack((I_interval, dI))
 
     assert I_interval.shape[0] == t and I_interval.shape[1] == 4, "I_interval should be {}x{}. Was: {}".format(t, 4, I_interval.shape)
     return I_interval
@@ -37,41 +37,41 @@ def get_model_activity(model, inputs):
     return s_lambdas
 
 
-def activity_RMSE(model_spikes, target_spikes, bin_size = 1250):
+def activity_RMSE(model_spikes, target_spikes, bin_size):
     num_pops = 4
     assert model_spikes.shape[1] == num_pops, "assuming row by column shape, i.e. (t, 4): {}".format(model_spikes.shape)
     assert target_spikes.shape[1] == num_pops, "assuming row by column shape, i.e. (t, 4): {}".format(target_spikes.shape)
     assert model_spikes.shape[0] % bin_size == 0, "time should be a multiple of bin_size: {}, model_spikes.shape: {}".format(bin_size, model_spikes.shape)
     num_bins = int(model_spikes.shape[0] / bin_size)
-    m_binned_spikes = np.zeros((num_bins, num_pops))
-    t_binned_spikes = np.zeros((num_bins, num_pops))
+    m_binned_spikes = torch.zeros((num_bins, num_pops))
+    t_binned_spikes = torch.zeros((num_bins, num_pops))
     for b_i in range(num_bins):
         for bp_i in range(num_pops):
             m_binned_spikes[b_i, bp_i] = model_spikes[b_i * bin_size:(b_i + 1) * bin_size, bp_i].sum()
             t_binned_spikes[b_i, bp_i] = target_spikes[b_i * bin_size:(b_i + 1) * bin_size, bp_i].sum()
-    rmse = np.sqrt(np.sum(np.mean(np.power(m_binned_spikes - t_binned_spikes, 2))) / num_pops)
-    return rmse
+    rmse = torch.sqrt(torch.sum(torch.mean(torch.pow(m_binned_spikes - t_binned_spikes, 2))) / num_pops)
+    return rmse.detach().numpy()
 
 
-def activity_correlations(model_act, tar_act, bin_size = 1250):
+def activity_correlations(model_act, tar_act, bin_size):
     num_pops = 4
     assert model_act.shape[1] == num_pops, "assuming row by column shape, i.e. (t, 4): {}".format(model_act.shape)
     assert tar_act.shape[1] == num_pops, "assuming row by column shape, i.e. (t, 4): {}".format(tar_act.shape)
     assert model_act.shape[0] % bin_size == 0, "time should be a multiple of bin_size: {}, m_act.shape: {}".format(bin_size, model_act.shape)
     num_bins = int(model_act.shape[0] / bin_size)
-    m_bins = np.zeros((num_bins, num_pops))
-    t_bins = np.zeros((num_bins, num_pops))
+    m_bins = torch.zeros((num_bins, num_pops))
+    t_bins = torch.zeros((num_bins, num_pops))
     for b_i in range(num_bins):
         for bp_i in range(num_pops):
             m_bins[b_i, bp_i] = model_act[b_i * bin_size:(b_i + 1) * bin_size, bp_i].sum()
             t_bins[b_i, bp_i] = tar_act[b_i * bin_size:(b_i + 1) * bin_size, bp_i].sum()
-    m_act_avg = np.mean(m_bins)
-    t_act_avg = np.mean(t_bins)
-    assert len(m_act_avg) == num_pops, "len mactavg {} should be numpops {}".format(len(m_act_avg), num_pops)
-    rho = 0.
+    m_bin_avg = torch.mean(m_bins, dim=0)
+    t_bin_avg = torch.mean(t_bins, dim=0)
+    assert len(m_bin_avg) == num_pops, "len mactavg {} should be numpops {}".format(len(m_bin_avg), num_pops)
+    rho = torch.tensor(0.)
     for p_i in range(num_pops):
-        rho+= (tar_act[:, p_i] - t_act_avg[p_i]) * (model_act[:, p_i] - m_act_avg[p_i]) / np.sqrt(np.power((tar_act[:, p_i] - t_act_avg[p_i]), 2) * np.power((model_act[:, p_i] - m_act_avg[p_i])))
-    return rho / num_pops
+        rho+= ((t_bins[:, p_i] - t_bin_avg[p_i]) * (m_bins[:, p_i] - m_bin_avg[p_i]) / torch.sqrt(torch.pow((t_bins[:, p_i] - t_bin_avg[p_i]), 2) * torch.pow((m_bins[:, p_i] - m_bin_avg[p_i]), 2))).sum()
+    return (rho / num_pops).detach().numpy()
 
 
 def convert_posterior_to_model_params_dict(model_class, posterior_params, target_class, target_points, N):
@@ -153,6 +153,7 @@ for sbi_res_file in files_sbi_res:
     # print('log_probability: {}'.format(log_probability))
 
     N_samples = 10
+    t = 2000
     print('Drawing the {} most likely samples..'.format(N_samples))
     posterior_params = posterior.sample((N_samples,), x=observation)
     print('\nposterior_params: {}'.format(posterior_params))
@@ -168,21 +169,21 @@ for sbi_res_file in files_sbi_res:
         init_model = model_class(parameters=init_params_dict, N=N)
     else:
         init_model = model_class(parameters=init_params_dict, N=N, neuron_types=programmatic_neuron_types)
-    burn_in_inputs = OU_process_input(t=9000)
-    _, _ = get_model_activity(init_model, burn_in_inputs)
-    _, _ = get_model_activity(target_model, burn_in_inputs)
-    eval_inputs = OU_process_input(t=10000)
-    t_act_OU = get_model_activity(target_model, eval_inputs)
-    init_act_OU = get_model_activity(init_model, eval_inputs)
+    burn_in_inputs = torch.rand((t,))
+    _ = get_model_activity(init_model, burn_in_inputs)
+    _ = get_model_activity(target_model, burn_in_inputs)
+    # eval_inputs = OU_process_input(t=t)
+    # t_act_OU = get_model_activity(target_model, eval_inputs)
+    # init_act_OU = get_model_activity(init_model, eval_inputs)
 
-    white_noise = torch.rand((10000,))
+    white_noise = torch.rand((t,))
     init_act_wn = get_model_activity(init_model, white_noise)
     t_act_wn = get_model_activity(target_model, white_noise)
 
-    init_correlations_OU_per_model_type[m_name] = activity_correlations(init_act_wn, t_act_wn)
-    init_activity_rmse_OU_per_model_type[m_name] = activity_RMSE(init_act_wn, t_act_wn)
-    init_correlations_wn_per_model_type[m_name] = activity_correlations(init_act_OU, t_act_OU)
-    init_activity_rmse_wn_per_model_type[m_name] = activity_RMSE(init_act_OU, t_act_OU)
+    init_correlations_wn_per_model_type[m_name] = activity_correlations(init_act_wn, t_act_wn, bin_size=int(t/8))
+    init_activity_rmse_wn_per_model_type[m_name] = activity_RMSE(init_act_wn, t_act_wn, bin_size=int(t/8))
+    # init_correlations_OU_per_model_type[m_name] = activity_correlations(init_act_OU, t_act_OU, bin_size=int(t/8))
+    # init_activity_rmse_OU_per_model_type[m_name] = activity_RMSE(init_act_OU, t_act_OU, bin_size=int(t/8))
 
     for s_i in range(N_samples):
         model_params = convert_posterior_to_model_params_dict(model_class, posterior_params[s_i], target_class=model_class, target_points=[], N=N)
@@ -195,21 +196,21 @@ for sbi_res_file in files_sbi_res:
             model = model_class(parameters=model_params, N=N, neuron_types=programmatic_neuron_types)
 
         # burn in:
-        burn_in_inputs = OU_process_input(t=9000)
+        burn_in_inputs = OU_process_input(t=t)
         _ = get_model_activity(model, burn_in_inputs)
-        eval_inputs = OU_process_input(t=10000)
+        eval_inputs = OU_process_input(t=t)
         m_act_OU = get_model_activity(model, eval_inputs)
 
-        white_noise = torch.rand((10000,))
+        white_noise = torch.rand((t,))
         m_act_wn = get_model_activity(model, white_noise)
 
-        activity_correlation_white_noise = activity_correlations(m_act_wn, t_act_wn)
-        activity_RMSE_white_noise = activity_RMSE(m_act_wn, t_act_wn)
-        activity_correlation_OU_process = activity_correlations(m_act_OU, t_act_OU)
-        activity_RMSE_OU_process = activity_RMSE(m_act_OU, t_act_OU)
+        activity_correlation_white_noise = activity_correlations(m_act_wn, t_act_wn, bin_size=int(t/8))
+        activity_RMSE_white_noise = activity_RMSE(m_act_wn, t_act_wn, bin_size=int(t/8))
+        # activity_correlation_OU_process = activity_correlations(m_act_OU, t_act_OU, bin_size=int(t/8))
+        # activity_RMSE_OU_process = activity_RMSE(m_act_OU, t_act_OU, bin_size=int(t/8))
 
-        correlations_OU_per_model_type[m_name].append(activity_correlation_OU_process)
-        activity_rmse_OU_per_model_type[m_name].append(activity_RMSE_OU_process)
+        # correlations_OU_per_model_type[m_name].append(activity_correlation_OU_process)
+        # activity_rmse_OU_per_model_type[m_name].append(activity_RMSE_OU_process)
         correlations_wn_per_model_type[m_name].append(activity_correlation_white_noise)
         activity_rmse_wn_per_model_type[m_name].append(activity_RMSE_white_noise)
 
@@ -224,13 +225,13 @@ for m_k in correlations_wn_per_model_type.keys():
     rmse_wn.append(np.mean(activity_rmse_wn_per_model_type[m_k]))
     rmse_wn_std.append(np.std(activity_rmse_wn_per_model_type[m_k]))
 
-    correlations_OU.append(np.mean(correlations_OU_per_model_type[m_k]))
-    corr_OU_std.append(np.std(correlations_OU_per_model_type[m_k]))
-    rmse_OU.append(np.mean(activity_rmse_OU_per_model_type[m_k]))
-    rmse_OU_std.append(np.std(activity_rmse_OU_per_model_type[m_k]))
+    # correlations_OU.append(np.mean(correlations_OU_per_model_type[m_k]))
+    # corr_OU_std.append(np.std(correlations_OU_per_model_type[m_k]))
+    # rmse_OU.append(np.mean(activity_rmse_OU_per_model_type[m_k]))
+    # rmse_OU_std.append(np.std(activity_rmse_OU_per_model_type[m_k]))
 
     init_corrs_wn.append(init_correlations_wn_per_model_type[m_k])
-    init_corrs_OU.append(init_correlations_OU_per_model_type[m_k])
+    # init_corrs_OU.append(init_correlations_OU_per_model_type[m_k])
 
     xticks.append(m_k.replace('microGIF', 'miGIF').replace('mesoGIF', 'meGIF'))
 
@@ -238,9 +239,9 @@ plot_exp_type = 'export_sbi'
 plot.bar_plot_neuron_rates(init_corrs_wn, correlations_wn, 0, corr_wn_std,
                            exp_type=plot_exp_type, uuid='all', fname='sbi_mean_p_dist_all.eps', xticks=xticks,
                            custom_legend=['Init. model', 'Posterior models'], ylabel='Avg. param dist.')
-plot.bar_plot_neuron_rates(init_corrs_OU, rmse_wn, 0., rmse_wn_std, plot_exp_type, 'all',
-                           custom_legend=['Init. model', 'Posterior models'],
-                           fname='plot_rates_all.eps', xticks=xticks,
-                           custom_colors=['Green', 'Magenta'])
+# plot.bar_plot_neuron_rates(init_corrs_OU, rmse_wn, 0., rmse_wn_std, plot_exp_type, 'all',
+#                            custom_legend=['Init. model', 'Posterior models'],
+#                            fname='plot_rates_all.eps', xticks=xticks,
+#                            custom_colors=['Green', 'Magenta'])
 
 # sys.exit()
