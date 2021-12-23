498eb797875397a52957b6c04daa7a118959d3d8
William <williapb@gmail.com>
2021-12-23 18:55:54 +0100

    (WIP) Marginals to 2D projection (SBI params)

--- analysis/sbi_posterior_plots.py
+++ analysis/sbi_posterior_plots.py
@@ -1,12 +1,9 @@
-import sys
-
 import numpy as np
+from sbi import analysis as analysis
 
-import plot
 from IO import *
-from Models.microGIF import microGIF
 from analysis import analysis_util
-from analysis.spike_train_matlab_export import simulate_and_save_model_spike_train
+from rate_based_dev_sbi_main_multi import transform_model_to_sbi_params
 
 
 def convert_posterior_to_model_params_dict(model_class, posterior_params, target_class, target_points, N):
@@ -82,53 +79,39 @@ for sbi_res_file in files_sbi_res:
     # log_probability = posterior.log_prob(samples, x=observation)
     # print('log_probability: {}'.format(log_probability))
 
-    N_samples = 20
-    print('Drawing the {} most likely samples..'.format(N_samples))
+    N_samples = 200
+    print('Plotting with {} samples..'.format(N_samples))
     posterior_params = posterior.sample((N_samples,), x=observation)
     print('\nposterior_params: {}'.format(posterior_params))
     tar_m_name = m_name
     if m_name == 'microGIF':
         tar_m_name = 'mesoGIF'
     target_model = analysis_util.get_target_model(tar_m_name)
+    GT_pts = transform_model_to_sbi_params(target_model)
 
-    for s_i in range(N_samples):
-        model_params = convert_posterior_to_model_params_dict(model_class, posterior_params[s_i], target_class=model_class, target_points=[], N=N)
-        programmatic_neuron_types = torch.ones((N,))
-        for n_i in range(int(2 * N / 3), N):
-            programmatic_neuron_types[n_i] = -1
-        if model_class is microGIF:
-            model = model_class(parameters=model_params, N=N)
-        else:
-            model = model_class(parameters=model_params, N=N, neuron_types=programmatic_neuron_types)
+    N_dim = target_model.N
+    weights_offset = N_dim**2-N_dim
+    p_labels = target_model.free_parameters
+    num_free_params = len(p_labels)
+    p_avgs_samples = torch.mean(posterior_params[:weights_offset], dim=1)
+    t_avgs = torch.mean(GT_pts[:weights_offset])
+    for p_i in range(len(p_labels)-1):
+        p_single_value_avg_samples = torch.mean(posterior_params[:,(weights_offset + p_i * N_dim):(weights_offset + (p_i + 1) * N_dim)], dim=1)
+        p_avgs_samples = torch.hstack((p_avgs_samples, p_single_value_avg_samples))
+        t_single_value_avg = torch.mean(GT_pts[(weights_offset + p_i * N_dim):(weights_offset + (p_i + 1) * N_dim)])
+        t_avgs = torch.hstack((t_avgs, t_single_value_avg))
 
-        mean_model_rate = analysis_util.get_mean_rate_for_model(model)
-        mean_param_dist = analysis_util.get_param_dist(model, target_model)
-        rates_per_model_type[m_name].append(mean_model_rate)
-        p_dists_per_model_type[m_name].append(mean_param_dist)
+    limits_low = torch.tensor([0.])
+    limits_high = torch.tensor([1.])
 
-        init_p_dist, _ = analysis_util.get_init_param_dist(target_model)
-        init_dists_per_model_type[m_name].append(init_p_dist)
+    for i in range(1, len(model_class.free_parameters)):
+        limits_low = torch.hstack((limits_low, torch.tensor(model_class.param_lin_constraints[i][0])))
+        limits_high = torch.hstack((limits_high, torch.tensor(model_class.param_lin_constraints[i][1])))
 
-        target_rate = analysis_util.get_mean_rate_for_model(target_model)
-        target_rate_per_m_t[m_name] = target_rate
-
-xticks = []; rates = []; rate_stds = []; dists = []; dist_stds = []; init_dists = []; init_dist_stds = []
-for m_k in rates_per_model_type.keys():
-    rates.append(np.mean(rates_per_model_type[m_k]))
-    rate_stds.append(np.std(rates_per_model_type[m_k]))
-    dists.append(np.mean(p_dists_per_model_type[m_k]))
-    dist_stds.append(np.std(p_dists_per_model_type[m_k]))
-    init_dists.append(np.mean(init_dists_per_model_type[m_k]))
-    init_dist_stds.append(np.std(init_dists_per_model_type[m_k]))
-    xticks.append(m_k.replace('microGIF', 'miGIF').replace('mesoGIF', 'meGIF'))
-
-plot_exp_type = 'export_sbi'
-plot.bar_plot_neuron_rates(init_dists, dists, init_dist_stds, dist_stds,
-                           exp_type=plot_exp_type, uuid='all', fname='sbi_mean_p_dist_all.eps', xticks=xticks,
-                           custom_legend=['Initial models', 'Fitted models'], ylabel='Avg. param dist.')
-plot.bar_plot_neuron_rates(list(target_rate_per_m_t.values()), rates, 0., rate_stds, plot_exp_type, 'all',
-                           custom_legend=['Target models', 'Fitted models'],
-                           fname='plot_rates_all.eps', xticks=xticks,
-                           custom_colors=['Green', 'Magenta'])
+    fig, ax = analysis.pairplot(samples=p_avgs_samples, points=t_avgs, limits=torch.stack((limits_low, limits_high), dim=1),
+                                figsize=(num_free_params, num_free_params))
+    if method is None:
+        method = dt_descriptor
+    fig.savefig('./figures/analysis_pairplot_{}_one_param_{}_{}.png'.format(method, m_name, dt_descriptor))
 
 # sys.exit()
