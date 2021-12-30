4e4ad317f319cd7d9c724a4d03a01ed955b6a15c
William <williapb@gmail.com>
2021-12-30 17:15:26 +0100

    If file exists in export, add paths new sleep and microGIF exps

--- analysis/export_matlab_data_for_dir_nuovo.py
+++ analysis/export_matlab_data_for_dir_nuovo.py
@@ -24,7 +24,9 @@ model_class_lookup = { 'LIF': LIF, 'GLIF': GLIF, 'microGIF': microGIF,
                        'LIF_no_cell_types': LIF_no_cell_types, 'GLIF_no_cell_types': GLIF_no_cell_types }
 
 # experiments_path = '/home/william/repos/snn_inference/Test/saved/GT/'
-experiments_path = '/media/william/p6/archive_14122021/archive/saved/sleep_data_no_types/'
+# experiments_path = '/media/william/p6/archive_14122021/archive/saved/sleep_data_no_types/'
+experiments_path = '/media/william/p6/archive_30122021_full/archive/saved/sleep_data_no_types/'  # GLIF, LIF
+# experiments_path = '/media/william/p6/archive_30122021_full/archive/saved/sleep_data/'  # microGIF / SGIF
 # archive_name = 'data/'
 # plot_data_path = experiments_path + 'plot_data/'
 model_type_dirs = os.listdir(experiments_path)
@@ -34,17 +36,21 @@ for model_type_str in model_type_dirs:
     if not model_type_str.__contains__("plot_data"):
         model_class = model_class_lookup[model_type_str]
         # model_class = microGIF
-        exp_uids = os.listdir(experiments_path + '/' + model_type_str)
-        for euid in exp_uids:
-            load_data = torch.load(experiments_path + '/' + model_type_str + '/' + euid + '/' + load_fname + IO.fname_ext)
-            snn = load_data['model']
-            # saved_target_losses = load_data['loss']
-            # num_neurons = snn.N
-            cur_fname = 'nuovo_sdnt_spikes_mt_{}_euid_{}'.format(model_class.__name__, euid)
+        if os.path.exists(experiments_path + model_type_str):
+            exp_uids = os.listdir(experiments_path + '/' + model_type_str)
+            for euid in exp_uids:
+                load_data = torch.load(experiments_path + '/' + model_type_str + '/' + euid + '/' + load_fname + IO.fname_ext)
+                snn = load_data['model']
+                # saved_target_losses = load_data['loss']
+                # num_neurons = snn.N
+                cur_fname = 'nuovo_sleep_v2_spikes_mt_{}_euid_{}'.format(model_class.__name__, euid)
 
-            if not os.path.exists(data_util.prefix + data_util.target_data_path + data_util.matlab_export + cur_fname + '.mat'):
-                simulate_and_save_model_spike_train(model=snn, t=duration, exp_num=euid, model_name=model_class.__name__, fname=cur_fname)
-            else:
-                print('file exists. skipping..')
+                if not os.path.exists(data_util.prefix + data_util.target_data_path + data_util.matlab_export + cur_fname + '.mat'):
+                    print('simulating data for: {}'.format(experiments_path + model_type_str))
+                    simulate_and_save_model_spike_train(model=snn, t=duration, exp_num=euid, model_name=model_class.__name__, fname=cur_fname)
+                else:
+                    print('file exists. skipping..')
+        else:
+            print('path does not exist: {}'.format(experiments_path + '/' + model_type_str))
 
 sys.exit()
